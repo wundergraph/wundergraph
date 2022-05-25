03ae3ed64d124edcb1dee538732699779058ff69
Jens Neuse <jens.neuse@gmx.de>
2022-05-25 10:52:46 +0200

    add postgres example

---
+++ examples/postgres/.wundergraph/wundergraph.config.ts
@@ -0,0 +1,48 @@
+import {
+	Application,
+	configureWunderGraphApplication,
+	cors,
+	introspect,
+	templates,
+} from '@wundergraph/sdk';
+import server from './wundergraph.server';
+import operations from './wundergraph.operations';
+
+const db = introspect.postgresql({
+	apiNamespace: "db",
+	databaseURL: "postgresql://admin:admin@localhost:54322/example?schema=public",
+});
+
+const myApplication = new Application({
+	name: 'app',
+	apis: [
+		db,
+	],
+});
+
+// configureWunderGraph emits the configuration
+configureWunderGraphApplication({
+	application: myApplication,
+	server,
+	operations,
+	codeGenerators: [
+		{
+			templates: [
+				...templates.typescript.all,
+				templates.typescript.operations,
+			],
+		},
+	],
+	cors: {
+		...cors.allowAll,
+		allowedOrigins: [
+			"http://localhost:3000"
+		]
+	},
+	dotGraphQLConfig: {
+		hasDotWunderGraphDirectory: false,
+	},
+	security: {
+		enableGraphQLEndpoint: true,
+	},
+});
