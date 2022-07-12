72a1605d8428dd527259e13adbc56c6e0ed313e2
Jens Neuse <jens.neuse@gmx.de>
2022-07-12 11:35:31 +0200

    fix: split user and id token due to size, add auth0 example (#71)
    * fix: split user and id token due to size, add auth0 example
    
    * fix: move resetting user cookies into own function
    
    * chore: address feedback
    
    * chore: remove unused import

---
+++ examples/auth0-oidc-authentication/.wundergraph/wundergraph.server.ts
@@ -0,0 +1,8 @@
+import { configureWunderGraphServer } from '@wundergraph/sdk';
+import type { HooksConfig } from './generated/wundergraph.hooks';
+import type { InternalClient } from './generated/wundergraph.internal.client';
+
+export default configureWunderGraphServer<HooksConfig, InternalClient>(() => ({
+	hooks: {},
+	graphqlServers: [],
+}));
