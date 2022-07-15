80ca2d6399a3d408e8b8a0f245a20ab53e6bbdc2
Dustin Deus <deusdustin@gmail.com>
2022-07-15 13:39:32 +0200

    fix: handle cli paths correctly (#81)

--- cli/commands/publish.go
+++ cli/commands/publish.go
@@ -8,6 +8,7 @@ import (
 	"strings"
 
 	"github.com/spf13/cobra"
+	"github.com/wundergraph/wundergraph/pkg/files"
 	"github.com/wundergraph/wundergraph/pkg/v2wundergraphapi"
 )
 
@@ -21,7 +22,11 @@ This command should be executed from your project root directory.
 The APIs to publish need to be generated into the .wundergraph/generated directory.`,
 	Example: `wunderctl publish organization/api`,
 	Args:    cobra.MinimumNArgs(1),
-	Run: func(cmd *cobra.Command, args []string) {
+	RunE: func(cmd *cobra.Command, args []string) error {
+		if !files.DirectoryExists(wundergraphDir) {
+			return fmt.Errorf("could not find wundergraph directory: %s", wundergraphDir)
+		}
+
 		var client *v2wundergraphapi.Client
 		if serviceToken != "" {
 			client = InitWunderGraphApiClientWithToken(serviceToken)
@@ -67,6 +72,8 @@ The APIs to publish need to be generated into the .wundergraph/generated directo
 
 			_, _ = green.Printf("API '%s' has been published successfully!\n", apiName)
 		}
+
+		return nil
 	},
 }
 
