407ddc5f1a03985fb74b7ac62da915ae769d046f
Eelco Wiersma <contact@pagebakers.nl>
2022-08-09 17:19:46 +0200

    fix: fixed imports in client template

--- packages/sdk/src/codegen/templates/typescript/client.template.ts
+++ packages/sdk/src/codegen/templates/typescript/client.template.ts
@@ -1,6 +1,8 @@
 //language=handlebars
 export const handlebarTemplate = `
-import { WunderGraphClient, ClientConfig } from '@wundergraph/sdk'
+import { WunderGraphClient, ClientConfig } from '@wundergraph/sdk/client'
+
+export type Role = {{{ roleDefinitions }}};
 
 export const defaultClientConfig: ClientConfig = {
     applicationHash: "{{applicationHash}}",
