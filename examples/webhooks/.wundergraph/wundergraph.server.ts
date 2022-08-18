524150e26cd55a4256895c72df28fcb6a5e75486
starptech <deusdustin@gmail.com>
2022-08-18 21:41:52 +0200

    chore: improve webhooks example

--- examples/webhooks/.wundergraph/wundergraph.server.ts
+++ examples/webhooks/.wundergraph/wundergraph.server.ts
@@ -12,19 +12,18 @@ import type { InternalClient } from './generated/wundergraph.internal.client';
 export default configureWunderGraphServer<HooksConfig, InternalClient, WebhooksConfig>(() => ({
 	webhooks: {
 		// Enable this if you configure this endpoint on Github.
+		// Don't forget to set the environment variable before starting your WunderNode
 		// github: {
 		// 	verifier: GithubWebhookVerifier(new EnvironmentVariable('GITHUB_SECRET')),
 		// },
-
 		// or generic
-
-		github: {
-			verifier: CreateWebhookVerifier({
-				kind: WebhookVerifierKind.HMAC_SHA256,
-				signatureHeaderPrefix: '',
-				secret: new EnvironmentVariable('YOUR_SECRET'),
-				signatureHeader: '',
-			}),
-		},
+		// github: {
+		// 	verifier: CreateWebhookVerifier({
+		// 		kind: WebhookVerifierKind.HMAC_SHA256,
+		// 		signatureHeaderPrefix: '',
+		// 		secret: new EnvironmentVariable('YOUR_SECRET'),
+		// 		signatureHeader: '',
+		// 	}),
+		// },
 	},
 }));
