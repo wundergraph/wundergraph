0020f05530df0641770f32e65f44eff10af5e202
Jens Neuse <jens.neuse@gmx.de>
2022-08-18 12:10:12 +0200

    chore: add example with user specific filters (#132)
    * chore: add example with user specific filters
    
    * chore: update README

---
+++ examples/nextjs-postgres-user-filters/.wundergraph/wundergraph.server.ts
@@ -0,0 +1,45 @@
+import { GraphQLObjectType, GraphQLSchema, GraphQLString } from 'graphql';
+import { configureWunderGraphServer } from '@wundergraph/sdk';
+import type { HooksConfig } from './generated/wundergraph.hooks';
+import type { InternalClient } from './generated/wundergraph.internal.client';
+
+export default configureWunderGraphServer<HooksConfig, InternalClient>(() => ({
+	hooks: {
+		queries: {
+			UserNodes: {
+				mutatingPreResolve: async ({ internalClient }) => {
+					const user = await internalClient.queries.UserByEmail({
+						input: { email: 'jens@wundergraph.com' },
+					});
+					return {
+						where: {
+							created_at: {
+								gt: user.data.db_findFirstUser.Filter.node_created_after,
+							},
+						},
+					};
+				},
+			},
+		},
+		mutations: {},
+	},
+	graphqlServers: [
+		{
+			serverName: 'gql',
+			apiNamespace: 'gql',
+			schema: new GraphQLSchema({
+				query: new GraphQLObjectType({
+					name: 'RootQueryType',
+					fields: {
+						hello: {
+							type: GraphQLString,
+							resolve() {
+								return 'world';
+							},
+						},
+					},
+				}),
+			}),
+		},
+	],
+}));
