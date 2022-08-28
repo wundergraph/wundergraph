c06e00701890d358fb4d77a42c17548f782c6550
Yuri Buerov <yuribuerov@gmail.com>
2022-08-28 17:15:57 +0200

    fix: s3 upload client, adjust minio setup script

--- scripts/minio-setup.sh
+++ scripts/minio-setup.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
-if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-	wget https://dl.min.io/client/mc/release/linux-amd64/mc -P scripts/
-elif [[ "$OSTYPE" == "darwin"* ]]; then
-	if [[ $(uname -p) == "arm" ]]; then
-		wget https://dl.min.io/client/mc/release/darwin-arm64/mc -P scripts/
-  elif [[ $(uname -p) == "i386" ]]; then  
-  	wget https://dl.min.io/client/mc/release/darwin-amd64/mc -P scripts/
-  fi
+set -e
+
+if ! [[ -x scripts/mc ]]; then
+	if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+		wget https://dl.min.io/client/mc/release/linux-amd64/mc -P scripts/
+	elif [[ "$OSTYPE" == "darwin"* ]]; then
+		if [[ $(uname -p) == "arm" ]]; then
+			wget https://dl.min.io/client/mc/release/darwin-arm64/mc -P scripts/
+		elif [[ $(uname -p) == "i386" ]]; then
+			wget https://dl.min.io/client/mc/release/darwin-amd64/mc -P scripts/
+		fi
+	fi
 fi
 
 chmod +x ./scripts/mc
@@ -17,5 +21,3 @@ chmod +x ./scripts/mc
 ./scripts/mc admin user info minio test
 ./scripts/mc mb --ignore-existing minio/uploads
 ./scripts/mc policy set public minio/uploads
-
-rm scripts/mc
