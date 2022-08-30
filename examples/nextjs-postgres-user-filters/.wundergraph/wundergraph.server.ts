59c0bf62658155a6b850a4fed0a81b792262ad97
Jens Neuse <jens.neuse@gmx.de>
2022-08-30 16:28:30 +0200

    chore: remove optional WebhookConfig

--- examples/nextjs-postgres-user-filters/.wundergraph/wundergraph.server.ts
+++ examples/nextjs-postgres-user-filters/.wundergraph/wundergraph.server.ts
@@ -2,9 +2,8 @@ import { GraphQLObjectType, GraphQLSchema, GraphQLString } from 'graphql';
 import { configureWunderGraphServer } from '@wundergraph/sdk';
 import type { HooksConfig } from './generated/wundergraph.hooks';
 import type { InternalClient } from './generated/wundergraph.internal.client';
-import type { WebhooksConfig } from './generated/wundergraph.webhooks';
 
-export default configureWunderGraphServer<HooksConfig, InternalClient, WebhooksConfig>(() => ({
+export default configureWunderGraphServer<HooksConfig, InternalClient>(() => ({
 	hooks: {
 		queries: {
 			UserNodes: {
