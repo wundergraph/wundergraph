bba47a6754b54bd5d6b0637ebbb9e2c53147b42d
Jens Neuse <jens.neuse@gmx.de>
2022-08-30 15:09:52 +0200

    chore: remove spacex api

--- examples/nextjs-postgres-user-filters/.wundergraph/wundergraph.server.ts
+++ examples/nextjs-postgres-user-filters/.wundergraph/wundergraph.server.ts
@@ -2,8 +2,9 @@ import { GraphQLObjectType, GraphQLSchema, GraphQLString } from 'graphql';
 import { configureWunderGraphServer } from '@wundergraph/sdk';
 import type { HooksConfig } from './generated/wundergraph.hooks';
 import type { InternalClient } from './generated/wundergraph.internal.client';
+import type { WebhooksConfig } from './generated/wundergraph.webhooks';
 
-export default configureWunderGraphServer<HooksConfig, InternalClient>(() => ({
+export default configureWunderGraphServer<HooksConfig, InternalClient, WebhooksConfig>(() => ({
 	hooks: {
 		queries: {
 			UserNodes: {
