c2b1edecece12f61289ff6f7116ffa756e24cfa0
starptech <deusdustin@gmail.com>
2022-09-02 21:56:38 +0200

    fix: only validate auth when set

--- pkg/validate/apiconfig.go
+++ pkg/validate/apiconfig.go
@@ -6,26 +6,27 @@ import (
 )
 
 func ApiConfig(api *wgpb.Api) (valid bool, messages []string) {
-	if api == nil || api.AuthenticationConfig == nil || api.AuthenticationConfig.CookieBased == nil {
-		return true, nil
-	}
-	if len(api.AuthenticationConfig.CookieBased.Providers) == 0 {
-		return true, nil
-	}
+	// if providers slice is empty, cookie based auth is disabled
+	if api != nil &&
+		api.AuthenticationConfig != nil &&
+		api.AuthenticationConfig.CookieBased != nil &&
+		len(api.AuthenticationConfig.CookieBased.Providers) > 0 {
 
-	csrfSecret := loadvariable.String(api.AuthenticationConfig.CookieBased.CsrfSecret)
-	if valid, msg := NewValidator("authentication.cookieBased.csrfTokenSecret", csrfSecret).Validate(Required(), Length(11)); !valid {
-		return valid, msg
-	}
+		csrfSecret := loadvariable.String(api.AuthenticationConfig.CookieBased.CsrfSecret)
+		if valid, msg := NewValidator("authentication.cookieBased.csrfTokenSecret", csrfSecret).Validate(Required(), Length(11)); !valid {
+			return valid, msg
+		}
 
-	blockKey := loadvariable.String(api.AuthenticationConfig.CookieBased.BlockKey)
-	if valid, msg := NewValidator("authentication.cookieBased.secureCookieBlockKey", blockKey).Validate(Required(), Length(32)); !valid {
-		return valid, msg
-	}
+		blockKey := loadvariable.String(api.AuthenticationConfig.CookieBased.BlockKey)
+		if valid, msg := NewValidator("authentication.cookieBased.secureCookieBlockKey", blockKey).Validate(Required(), Length(32)); !valid {
+			return valid, msg
+		}
 
-	hashKey := loadvariable.String(api.AuthenticationConfig.CookieBased.HashKey)
-	if valid, msg := NewValidator("authentication.cookieBased.secureCookieHashKey", hashKey).Validate(Required(), Length(32)); !valid {
-		return valid, msg
+		hashKey := loadvariable.String(api.AuthenticationConfig.CookieBased.HashKey)
+		if valid, msg := NewValidator("authentication.cookieBased.secureCookieHashKey", hashKey).Validate(Required(), Length(32)); !valid {
+			return valid, msg
+		}
+
 	}
 
 	return true, nil
