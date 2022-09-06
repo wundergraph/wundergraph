e9673b39596702ddf1d2d32f1e5ff868f9874b8b
starptech <deusdustin@gmail.com>
2022-09-06 11:20:45 +0200

    chore: test

--- scripts/pnpm.sh
+++ scripts/pnpm.sh
@@ -4,11 +4,9 @@ set -e
 
 # Bootstrap pnpm workspace
 
-# Install wunderctl without scripts because wunderctl has to be build first
-pnpm install --filter="./packages/wunderctl" --ignore-scripts
+# Installwithout scripts because wunderctl has to be build first
+pnpm install --ignore-scripts
 # Build wunderctl
 pnpm -r run --filter="./packages/wunderctl" build
-# Download wunderctl
-pnpm -r run --filter="./packages/wunderctl" postinstall
-# Install, link all packages + build all packages
-pnpm install --ignore-scripts && pnpm build:libs
+# Build all libs + link all packages
+pnpm build:libs && pnpm i
