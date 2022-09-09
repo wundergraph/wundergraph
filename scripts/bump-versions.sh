61d321ac116c11b1cf2445667bde532713329900
starptech <deusdustin@gmail.com>
2022-09-09 23:38:30 +0200

    chore: update versions

---
+++ scripts/bump-versions.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+set -e
+
+printf "{\"name\": \"examples\"}" > examples/package.json
+printf "packages:\n  - '*'" > examples/pnpm-workspace.yaml
+cd examples && pnpm up -r "@wundergraph/sdk@latest" "@wundergraph/nextjs@latest"
+rm package.json pnpm-workspace.yaml pnpm-lock.yaml
