dcf304a420e7ee916e58aa9f9fbc69fc93fead1b
Eelco Wiersma <contact@pagebakers.nl>
2022-09-16 08:57:23 +0200

    chore: remove unused code

--- packages/sdk/src/codegen/templates/typescript/react.ts
+++ packages/sdk/src/codegen/templates/typescript/react.ts
@@ -81,19 +81,6 @@ export class TypescriptReactHooks implements Template {
 	}
 }
 
-// export const isNotInternal = (op: GraphQLOperation): boolean => !op.Internal;
-
-// export const hasInput = (op: GraphQLOperation): boolean =>
-// 	op.VariablesSchema.properties !== undefined && Object.keys(op.VariablesSchema.properties).length !== 0;
-
-// export const hasInternalInput = (op: GraphQLOperation): boolean =>
-// 	op.InternalVariablesSchema.properties !== undefined &&
-// 	Object.keys(op.InternalVariablesSchema.properties).length !== 0;
-
-// export const hasInjectedInput = (op: GraphQLOperation): boolean =>
-// 	op.InjectedVariablesSchema.properties !== undefined &&
-// 	Object.keys(op.InjectedVariablesSchema.properties).length !== 0;
-
 const mustOperationTypeQuery = (op: GraphQLOperation) => op.OperationType === OperationType.QUERY;
 const mustLiveQuery = (op: GraphQLOperation) => op.LiveQuery !== undefined && op.LiveQuery.enable;
 const mustOperationTypeMutation = (op: GraphQLOperation) => op.OperationType === OperationType.MUTATION;
