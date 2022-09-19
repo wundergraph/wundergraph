3a84f6c3e3a6371c06527026b7ed78f5867e8c28
Sergey Petrunin <spetrunin@users.noreply.github.com>
2022-09-19 18:29:11 +0300

    chore: revert protobuf changes for open api base url as a string (#190)
    * chore: revert protobuf changes for open api base url as a string
    
    * chore: simplify obtaining base url

--- packages/sdk/src/v2openapi/index.ts
+++ packages/sdk/src/v2openapi/index.ts
@@ -214,6 +214,9 @@ class RESTApiBuilder {
 			return;
 		}
 		const parentType = verb === HTTPMethod.GET ? 'Query' : 'Mutation';
+
+		const baseUrl = this.cleanupBaseURL(this.baseURL());
+
 		this.dataSources.push({
 			RootNodes: [
 				{
@@ -226,7 +229,7 @@ class RESTApiBuilder {
 				Fetch: {
 					method: verb,
 					path: mapInputVariable(path),
-					baseUrl: this.baseURL(),
+					baseUrl: mapInputVariable(baseUrl),
 					url: mapInputVariable(''),
 					body: mapInputVariable(''),
 					header: this.headers,
@@ -244,10 +247,7 @@ class RESTApiBuilder {
 			ChildNodes: [],
 			Directives: [],
 		});
-		if (this.dataSources[this.dataSources.length - 1].Custom.Fetch.baseUrl) {
-			const baseURL = this.dataSources[this.dataSources.length - 1].Custom.Fetch.baseUrl!;
-			this.dataSources[this.dataSources.length - 1].Custom.Fetch.baseUrl = this.cleanupBaseURL(baseURL);
-		}
+
 		this.fields.push({
 			typeName: parentType,
 			fieldName: fieldName,
