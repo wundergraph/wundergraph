43cf08d446d17d79af611f6648f5357d6c007005
starptech <deusdustin@gmail.com>
2022-09-20 20:13:51 +0200

    fix(swr): reset user cache after logout

--- packages/swr/src/hooks.ts
+++ packages/swr/src/hooks.ts
@@ -137,8 +137,8 @@ export const createHooks = <Operations extends OperationsDefinition>(client: Cli
 				client.login(authProviderID, redirectURI),
 			logout: async (options?: LogoutOptions | undefined) => {
 				const result = await client.logout(options);
-				// refetch user
-				mutate(userSWRKey);
+				// reset user
+				mutate(userSWRKey, null, { populateCache: true });
 				return result;
 			},
 		};
