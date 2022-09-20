e6081444651a376f50e4b629b47c734edabc71d8
starptech <deusdustin@gmail.com>
2022-09-20 20:21:34 +0200

    chore: improve

--- packages/swr/src/hooks.ts
+++ packages/swr/src/hooks.ts
@@ -137,8 +137,8 @@ export const createHooks = <Operations extends OperationsDefinition>(client: Cli
 				client.login(authProviderID, redirectURI),
 			logout: async (options?: LogoutOptions | undefined) => {
 				const result = await client.logout(options);
-				// reset user
-				mutate(userSWRKey, null, { revalidate: false, optimisticData: null, rollbackOnError: false });
+				// reset user in the cache and don't trigger a refetch
+				mutate(userSWRKey, null, { revalidate: false });
 				return result;
 			},
 		};
