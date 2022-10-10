wundergraph/wundergraph

commit e2468c8856e02a7d1d89dc1c08c1731871bc19f3

feat: replace the legacy client with the new implementation (#78)
* feat: add new Wundergraph client package

* feat: remove client from nextjs package

* feat: renamed WundergraphClient to WunderGraphClient

* chore: updated readme

* chore: update package description and removed unused dependencies

* feat: moved TypeScript client into sdk package

* feat: add pass down abortSignal to query and mutation fetch call

* feat: replaced the legacy client

* chore: use exports to export client

* chore: added readme

* docs: improved uploads example

* feat: improved client types and response handling

* feat: add dist folder to exports

* chore: no need to use default

* fix: add types to exports

* fix: return correct upload error response

* feat: removed requiresAuthentication from client

* docs: added subscription example

* docs: fix subscription examples

* chore: update lockfile

* feat: make sure customFetch is used as a fallback everywhere

* fix: remove ts-ignore and fix ts errors

* fix: throw fetchUser if there is a request error

* feat: improved client api

* chore: make sure auth works out of the box in nextjs-testapp

* fix: make sure the response type is correct

* chore: remove commented types

Co-authored-by: Dustin Deus <deusdustin@gmail.com>

packages/sdk/src/codegen/templates/typescript/web.client.template.ts
@@ -1,8 +1,10 @@
 //language=handlebars
 export const template = `
 import type { {{ modelImports }} } from "./models";
-import type { RequestOptions, ClientConfig, UserListener, Response, FetchConfig, Headers } from "@wundergraph/sdk";
-import type { User } from "./wundergraph.server";
+import type { RequestOptions, UserListener, Response } from "@wundergraph/sdk";
+import type { User, Role } from "./wundergraph.server";
+
+import { WunderGraphClient, ClientConfig, UploadConfig as BaseUploadConfig, QueryResult, MutationResult, SubscriptionResult } from "@wundergraph/sdk/client";
 
 export const WUNDERGRAPH_S3_ENABLED = {{hasS3Provider}};
 export const WUNDERGRAPH_AUTH_ENABLED = {{hasAuthProviders}};
@@ -10,19 +12,13 @@ export const WUNDERGRAPH_AUTH_ENABLED = {{hasAuthProviders}};
 {{#if hasS3Provider}}
 export interface UploadResponse { key: string }
 
-export interface UploadConfig {
-    provider: S3Provider;
-    formData: FormData;
-    abortSignal?: AbortSignal;
-}
-{{/if}}
-
-{{#if hasS3Provider}}
 export enum S3Provider {
     {{#each s3Provider }}
     "{{name}}" = "{{name}}",
     {{/each}}
 }
+
+export type UploadConfig = BaseUploadConfig<S3Provider>
 {{/if}}
 
 {{#if hasAuthProviders}}
@@ -42,27 +38,31 @@ export interface LogoutOptions {
     logout_openid_connect_provider?: boolean;
 }
 
+type Result<Data> = QueryResult<Data> | MutationResult<Data> | SubscriptionResult<Data>;
+
 export class Client {
-    constructor(config?: ClientConfig) {
-        this.baseURL = config?.baseURL || this.baseURL;
-        this.extraHeaders = config?.extraHeaders;
+    constructor(config: Partial<ClientConfig> = {}) {
+        const {
+            baseURL = "{{ baseURL }}",
+            ...rest
+        } = config
+        
+        this._client = new WunderGraphClient<Role>({
+            baseURL,
+            applicationHash: "{{ applicationHash }}",
+            applicationPath: "{{ applicationPath }}",
+            sdkVersion: "{{ sdkVersion }}",
+            ...rest
+        });
+
         this.user = null;
-        this.customFetch = config?.customFetch;
     }
+    private _client: WunderGraphClient<Role>;
     private logoutCallback: undefined | (() => void);
     public setLogoutCallback(cb: ()=>void){
         this.logoutCallback = cb;
     }
-    public setExtraHeaders = (headers: Headers) => {
-        this.extraHeaders = headers;
-    }
-    private customFetch?: ((input: RequestInfo, init?: RequestInit) => Promise<globalThis.Response>);
-    private extraHeaders?: Headers;
-    private readonly baseURL: string = "{{ baseURL }}";
-    private readonly applicationHash: string = "{{ applicationHash }}"
-    private readonly applicationPath: string = "{{ applicationPath }}"
-    private readonly sdkVersion: string = "{{ sdkVersion }}"
-    private csrfToken: string | undefined;
+
     private user: User | null;
     private userListener: UserListener<User> | undefined;
     public setUserListener = (listener: UserListener<User>) => {
@@ -80,16 +80,48 @@ export class Client {
             }
         }
     };
+	private resultToResponse = <TResponse>(result: Result<any>): Response<TResponse> => {
+		switch (result.status) {
+			case 'ok':
+				return {
+					status: 'ok',
+					body: { data: result.data }
+				} as Response<any>
+			case 'error':
+				return {
+					status: 'error',
+					message: result.errors[0].message,
+				}
+			case 'requires_authentication':
+				return {
+					status: 'requiresAuthentication'
+				}
+			case 'cancelled':
+				return {
+					status: 'aborted'
+				}
+			case 'partial': {
+				return {
+					status: 'error',
+					message: result.errors[0].message,
+				};
+			}
+			default:
+				return {
+					status: result.status
+				}
+		}
+	};
     {{#if hasQueries}}
     public query = {
         {{#each queries}}
         {{operationName}}: async (options: RequestOptions<{{#if hasInput}}{{operationName}}Input{{else}}never{{/if}},{{operationName}}Response>) => {
-            return await this.doFetch<{{operationName}}Response>({
-                method: "GET",
-                path: "{{path}}",
+            const result = await this._client.query({
+                operationName: "{{operationName}}",
                 input: options.input,
                 abortSignal: options.abortSignal,
             })
+            return this.resultToResponse<{{operationName}}Response>(result)
         },
         {{/each}}
     }
@@ -98,12 +130,12 @@ export class Client {
     public mutation = {
     {{#each mutations}}
         {{operationName}}: async (options: RequestOptions<{{#if hasInput}}{{operationName}}Input{{else}}never{{/if}},{{operationName}}Response>) => {
-            return await this.doFetch<{{operationName}}Response>({
-                method: "POST",
-                path: "{{path}}",
+            const result =  await this._client.mutate({
+                operationName: "{{operationName}}",
                 input: options.input,
                 abortSignal: options.abortSignal,
             })
+            return this.resultToResponse<{{operationName}}Response>(result)
         },
     {{/each}}
     }
@@ -112,12 +144,12 @@ export class Client {
     public subscription = {
     {{#each subscriptions}}
         {{operationName}}: (options: RequestOptions<{{#if hasInput}}{{operationName}}Input{{else}}never{{/if}},{{operationName}}Response>, cb: (response: Response<{{operationName}}Response>) => void) => {
-            return this.startSubscription<{{operationName}}Response>({
-                method: "GET",
-                path: "{{path}}",
+            return this._client.subscribe({
+                operationName: "{{operationName}}",
+                isLiveQuery: false,
                 input: options.input,
                 abortSignal: options.abortSignal,
-            },cb);
+            }, (result) => cb(this.resultToResponse<{{operationName}}Response>(result)));
         },
     {{/each}}
     }
@@ -126,250 +158,58 @@ export class Client {
         public liveQuery = {
         {{#each liveQueries }}
             {{operationName}}: (options: RequestOptions<{{#if hasInput}}{{operationName}}Input{{else}}never{{/if}},{{operationName}}Response>, cb: (response: Response<{{operationName}}Response>) => void) => {
-                return this.startSubscription<{{operationName}}Response>({
-                    method: "GET",
-                    path: "{{path}}",
+                return this._client.subscribe({
+                    operationName: "{{operationName}}",
+                    isLiveQuery: true,
                     input: options.input,
                     abortSignal: options.abortSignal,
-                    liveQuery: true,
-                },cb);
+                }, (result) => cb(this.resultToResponse<{{operationName}}Response>(result)));
             },
         {{/each}}
         }
     {{/unless}}{{/if}}
+
     {{#if hasS3Provider}}
     public uploadFiles = async (config: UploadConfig): Promise<Response<UploadResponse[]>> => {
-        try {
-            // pass only files
-            config.formData.forEach((value, key) => {
-							if (!(value instanceof Blob)){
-							delete config.formData[key];
-						}
-			});
-            const baseHeaders: Headers = {
-                ...this.extraHeaders,
-                "WG-SDK-Version": this.sdkVersion,
-            };
-            const params = this.queryString({
-                wg_api_hash: this.applicationHash,
-            })
-            if (this.csrfToken === undefined) {
-                const f = this.customFetch || fetch;
-                const res = await f(this.baseURL + "/" + this.applicationPath + "/auth/cookie/csrf", {
-                    headers: {
-                        ...baseHeaders,
-                        Accept: "text/plain",
-                    },
-                    credentials: "include",
-                    mode: "cors",
-                });
-                this.csrfToken = await res.text();
-            }
-            const headers: Headers = {
-                ...baseHeaders,
-                Accept: "application/json",
-                "WG-SDK-Version": this.sdkVersion,
-            };
-            if (this.csrfToken) {
-                headers["X-CSRF-Token"] = this.csrfToken;
-            }
-            const body = config.formData;
-            const f = this.customFetch || fetch;
-            const res = await f(
-                this.baseURL + "/" + this.applicationPath + "/s3/" + config.provider + "/upload" + params,
-                {
-                    headers,
-                    body,
-                    method: "POST",
-                    signal: config.abortSignal,
-                    credentials: "include",
-                    mode: "cors",
-                }
-            );
-            if (res.status === 200) {
-                const json = await res.json();
-                return {
-                    status: "ok",
-                    body: json,
-                };
-            }
-            throw new Error(\`could not upload files, status: \${res.status}\`);
-        } catch (e: any) {
-            return {
-                status: "error",
-                message: e.toString(),
-            };
-        }
+        const result = await this._client.uploadFiles(config);
+
+		if (result.status === "ok") {
+			return {
+				status: "ok",
+				body: result.fileKeys.map((key) => ({key})),
+			};
+		}
+
+        return {
+			status: result.status,
+			message: result.errors[0].message
+		};
     };
     {{/if}}
-    private doFetch = async <T>(fetchConfig: FetchConfig): Promise<Response<T>> => {
-        try {
-            const params =
-                fetchConfig.method !== "POST"
-                    ? this.queryString({
-                        wg_variables: fetchConfig.input,
-                        wg_api_hash: this.applicationHash,
-                    })
-                    : "";
-            if (fetchConfig.method === "POST" && this.csrfToken === undefined) {
-                const f = this.customFetch || fetch;
-                const res = await f(this.baseURL + "/" + this.applicationPath + "/auth/cookie/csrf", {
-                    credentials: "include",
-                    mode: "cors",
-                });
-                this.csrfToken = await res.text();
-            }
-            const headers: Headers = {
-                ...this.extraHeaders,
-                Accept: "application/json",
-                "WG-SDK-Version": this.sdkVersion,
-            };
-            if (fetchConfig.method === "POST") {
-                if (this.csrfToken) {
-                    headers["X-CSRF-Token"] = this.csrfToken;
-                }
-            }
-            const body = fetchConfig.method === "POST" ? JSON.stringify(fetchConfig.input) : undefined;
-            const data = await this.fetch(
-                this.baseURL + "/" + this.applicationPath + "/operations/" + fetchConfig.path + params,
-                {
-                    headers,
-                    body,
-                    method: fetchConfig.method,
-                    signal: fetchConfig.abortSignal,
-                    credentials: "include",
-                    mode: "cors",
-                }
-            );
-            return {
-                status: "ok",
-                body: data,
-            };
-        } catch (e: any) {
-            return {
-                status: "error",
-                message: e,
-            };
-        }
-    };
-    private fetch = async (input: globalThis.RequestInfo, init?: RequestInit): Promise<any> => {
-				const makeRequest = this.customFetch || fetch;
-				const res = await makeRequest(input,init);
-				
-				if (res.status === 200){
-						return res.json();
-				} else if (res.status === 400) {
-						throw new Error("bad request");
-				} else if (res.status >= 401 && res.status <= 499){
-						this.csrfToken = undefined;
-						// invalidate user session
-						this.fetchUser().catch(() => console.error("failed to invalidate user session"));
-						throw new Error("unauthorized");
-				} else if (res.status >= 500 && res.status <= 599){
-						throw new Error("server error");
-				}
-				
-				throw new Error(\`unhandled status code: \${res.status}\`);
-    }
-    {{#if hasSubscriptionsOrLiveQueries }}{{#unless reactNative}}
-    private startSubscription = <T>(fetchConfig: FetchConfig, cb: (response: Response<T>) => void) => {
-        (async () => {
-            try {
-                const params = this.queryString({
-                    wg_variables: fetchConfig.input,
-                    wg_live: fetchConfig.liveQuery === true ? true : undefined,
-                });
-                const f = this.customFetch || fetch;
-                const response = await f(this.baseURL + "/" + this.applicationPath + "/operations/" + fetchConfig.path + params, {
-                    headers: {
-                        ...this.extraHeaders,
-                        "Content-Type": "application/json",
-                        "WG-SDK-Version": this.sdkVersion,
-                    },
-                    method: fetchConfig.method,
-                    signal: fetchConfig.abortSignal,
-                    credentials: "include",
-                    mode: "cors",
-                });
-                if (response.status === 401){
-                    this.csrfToken = undefined;
-                    return;
-                }
-                if (response.status !== 200 || response.body == null) {
-                    return
-                }
-                const reader = response.body.getReader();
-                const decoder = new TextDecoder();
-                let message: string = "";
-                while (true) {
-                    const {value, done} = await reader.read()
-                    if (done) break;
-                    if (!value) continue;
-                    message += decoder.decode(value);
-                    if (message.endsWith("\\n\\n")) {
-                        cb({
-                            status: "ok",
-                            body: JSON.parse(message.substring(0, message.length - 2)),
-                        });
-                        message = "";
-                    }
-                }
-            } catch (e: any) {
-                cb({
-                    status: "error",
-                    message: e,
-                });
-            }
-        })();
-    }
-    {{/unless}}{{/if}}
-    private queryString = (input?: Object): string => {
-        if (!input) {
-            return ""
-        }
-        const query = (Object.keys(input) as Array<keyof typeof input>)
-            // @ts-ignore
-            .filter(key => input[key] !== undefined && input[key] !== "")
-            .map(key => {
-                const value = typeof input[key] === "object" ? JSON.stringify(input[key]) : input[key];
-                const encodedKey = encodeURIComponent(key);
-                // @ts-ignore
-                const encodedValue = encodeURIComponent(value);
-                return \`\${encodedKey}=\${encodedValue}\`
-            }).join("&");
-        return query === "" ? query : "?" + query;
-    }
+   
     public fetchUser = async (revalidate?: boolean): Promise<User | null> => {
-        try {
-            const revalidateTrailer = revalidate === undefined ? "" : "?revalidate=true"
-            const f = this.customFetch || fetch;
-            const response = await f(this.baseURL + "/" + this.applicationPath + "/auth/cookie/user" + revalidateTrailer, {
-                headers: {
-                    ...this.extraHeaders,
-                    "Content-Type": "application/json",
-                    "WG-SDK-Version": this.sdkVersion,
-                },
-                method: "GET",
-                credentials: "include",
-                mode: "cors",
-            });
-            if (response.status === 200) {
-                const user = await response.json();
-                this.setUser(user);
-                return this.user;
-            }
-        } catch {}
-        this.setUser(null);
-        return null;
+		try {
+			const user = await this._client.fetchUser(null, revalidate);
+
+			if (user) {
+				this.setUser(user);
+				return this.user;
+			}
+		} catch {}
+
+		this.setUser(null);
+		return null;
     };
+
     {{#if hasAuthProviders}}
     public login : Record<AuthProviderId, AuthProvider['login']> = {
         {{#each authProviders}}
         {{.}}: (redirectURI?: string): void => {
-            this.startLogin(AuthProviderId.{{.}},redirectURI)
+            this._client.login(AuthProviderId.{{.}}, redirectURI)
         },
         {{/each}}
     }
+
     public authProviders: Array<AuthProvider> = [
         {{#each authProviders}}
         {
@@ -378,29 +218,14 @@ export class Client {
         },
         {{/each}}
     ]
-    public logout = async (options?: LogoutOptions) :Promise<boolean> => {
-        const f = this.customFetch || fetch;
-        const response = await f(this.baseURL + "/" + this.applicationPath + "/auth/cookie/user/logout" + this.queryString(options), {
-            headers: {
-                ...this.extraHeaders,
-                "Content-Type": "application/json",
-                "WG-SDK-Version": this.sdkVersion,
-            },
-            method: "GET",
-            credentials: "include",
-            mode: "cors",
-        });
+
+    public logout = async (options?: LogoutOptions): Promise<boolean> => {
+        const response = await this._client.logout(options);
+
         this.setUser(null);
-        if (this.logoutCallback){
-            this.logoutCallback();
-        }
-        return response.status === 200
-    }
-    private startLogin = (providerID: AuthProviderId, redirectURI?: string) => {
-        const query = this.queryString({
-            redirect_uri: redirectURI || window.location.toString(),
-        });
-        window.location.replace(\`\${this.baseURL}/\${this.applicationPath}/auth/cookie/authorize/\${providerID}\${query}\`);
+        this.logoutCallback?.()
+
+        return response
     }
     {{/if}}
 }

packages/sdk/src/client/index.ts
@@ -95,6 +95,7 @@ export class WunderGraphClient<Role> {
 			const response = await this.fetch(url, {
 				headers,
 				method: 'GET',
+				signal: query.abortSignal,
 			});
 			return this.httpResponseToQueryResult(response);
 		} catch (e: any) {
@@ -129,10 +130,11 @@ export class WunderGraphClient<Role> {
 				headers['X-CSRF-Token'] = this.csrfToken;
 			}
 			const url = this.baseURL + '/' + this.applicationPath + '/operations/' + mutation.operationName + params;
-			const body = mutation?.input !== undefined ? JSON.stringify(mutation.input) : '{}';
+			const body = mutation.input !== undefined ? JSON.stringify(mutation.input) : '{}';
 			const response = await this.fetch(url, {
 				headers,
 				method: 'POST',
+				signal: mutation.abortSignal,
 				body,
 			});
 			return this.httpResponseToMutationResult(response);
@@ -508,21 +510,9 @@ export interface QueryArgsWithInput<Input> extends QueryArgs {
 	input: Input;
 }
 
-// export interface QueryProps<Input, Args = QueryArgs> {
-// 	operationName: string;
-// 	input?: Input;
-// 	options?: Args;
-// }
-
 export type QueryProps<Args extends QueryArgs = QueryArgs> = Args & {
 	operationName: string;
 };
-
-// export interface SubscriptionProps<Input, Args = SubscriptionArgs> {
-// 	operationName: string;
-// 	input?: Input;
-// 	options?: Args;
-// }
 
 export type SubscriptionProps<Args extends SubscriptionArgs = SubscriptionArgs> = Args & {
 	operationName: string;
@@ -547,12 +537,6 @@ export interface InternalSubscriptionArgsWithInput<Input> extends InternalSubscr
 export interface SubscriptionArgsWithInput<Input> extends SubscriptionArgs {
 	input: Input;
 }
-
-// export interface MutationProps<Input, Args = MutationArgs> {
-// 	operationName: string;
-// 	input?: Input;
-// 	options?: Args;
-// }
 
 export type MutationProps<Args extends MutationArgs = MutationArgs> = Args & {
 	operationName: string;

packages/sdk/src/codegen/templates/typescript/web.client.ts
@@ -65,6 +65,7 @@ export const operations = (application: ResolvedApplication, operationType: Oper
 				path: op.Name,
 				hasInput: hasInput(op),
 				hasInternalInput: hasInternalInput(op),
+				requiresAuthentication: op.AuthenticationConfig.required,
 			};
 		});
 
@@ -77,6 +78,7 @@ export const liveQueries = (application: ResolvedApplication, includeInternal: b
 				path: op.Name,
 				hasInput: hasInput(op),
 				hasInternalInput: hasInternalInput(op),
+				requiresAuthentication: op.AuthenticationConfig.required,
 			};
 		});
 

commit dce978b18c86527f244c7e20105ecd825fa46d00

fix: live query support in generated web client
This regression was introduced in #78. The client SDK expects this
properted to be `liveQuery`.

packages/sdk/src/codegen/templates/typescript/web.client.template.ts
@@ -120,7 +120,7 @@ export class Client {
         {{operationName}}: (options: RequestOptions<{{#if hasInput}}{{operationName}}Input{{else}}never{{/if}},{{operationName}}Response>, cb: (response: Response<{{operationName}}Response>) => void) => {
             return this._client.subscribe({
                 operationName: "{{operationName}}",
-                isLiveQuery: false,
+                liveQuery: false,
                 input: options.input,
                 abortSignal: options.abortSignal,
             }, (result) => cb(this.resultToResponse<{{operationName}}Response>(result)));
@@ -134,7 +134,7 @@ export class Client {
             {{operationName}}: (options: RequestOptions<{{#if hasInput}}{{operationName}}Input{{else}}never{{/if}},{{operationName}}Response>, cb: (response: Response<{{operationName}}Response>) => void) => {
                 return this._client.subscribe({
                     operationName: "{{operationName}}",
-                    isLiveQuery: true,
+                    liveQuery: true,
                     input: options.input,
                     abortSignal: options.abortSignal,
                 }, (result) => cb(this.resultToResponse<{{operationName}}Response>(result)));