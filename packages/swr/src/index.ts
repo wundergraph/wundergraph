c05b341f64123d828caec4c95560163ce868ccd4
Eelco Wiersma <contact@pagebakers.nl>
2022-10-17 11:25:10 +0200

    feat: re-export SWR to prevent context issues (#264)
    * feat: re-export SWR
    
    * chore: remove refreshInterval

--- packages/swr/src/index.ts
+++ packages/swr/src/index.ts
@@ -1,3 +1,11 @@
+import useSWR from 'swr';
+
 export { createHooks } from './hooks';
 
 export type { UseMutationOptions, UseSubscriptionOptions, MutateOptions, UseQueryOptions } from './hooks';
+
+export { SWRConfig, useSWRConfig } from 'swr';
+
+export type { SWRConfiguration } from 'swr';
+
+export { useSWR };
