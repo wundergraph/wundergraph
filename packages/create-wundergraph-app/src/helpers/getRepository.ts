63ef5be1f2d0b096a80b47d9761c633d68bd1e98
Suvij Surya <suvijsurya76@gmail.com>
2022-11-11 02:22:36 +0530

    refactor: refactored getRepository function

--- packages/create-wundergraph-app/src/helpers/getRepository.ts
+++ packages/create-wundergraph-app/src/helpers/getRepository.ts
@@ -9,6 +9,65 @@ import { checkIfValidExample, getExamplesList } from './examples';
 import { getRepoInfo } from './getRepoInfo';
 import { validateBranch } from './validateBranch';
 
+const resolveRepository = async ({ exampleName, githubLink }: { exampleName?: string; githubLink?: string }) => {
+	try {
+		if (!exampleName && !githubLink) {
+			const examples = await getExamplesList();
+			const selectedExampleName = await inquirer.prompt({
+				name: 'selectExample',
+				type: 'list',
+				message: 'Which example would you like to start with?',
+				choices: [...examples],
+			});
+			return {
+				repoOwnerName: 'wundergraph',
+				repoName: 'wundergraph',
+				branch: 'main',
+				filePath: `examples/${selectedExampleName['selectExample']}`,
+			};
+		}
+		if (exampleName) {
+			const selectedExampleName = await checkIfValidExample(exampleName);
+			return {
+				repoOwnerName: 'wundergraph',
+				repoName: 'wundergraph',
+				branch: 'main',
+				filePath: `examples/${selectedExampleName}`,
+			};
+		} else if (githubLink) {
+			const { repoName, repoOwnerName, branch, filePath } = await getRepoInfo(githubLink);
+			let selectedBranchName = branch;
+			if (branch === '') {
+				const branchPrompt = await inquirer.prompt({
+					name: 'branch',
+					type: 'input',
+					message: 'Which branch of the repository would you like to use?',
+					default: 'main',
+				});
+				selectedBranchName = branchPrompt['branch'];
+				const isBranchValid = await validateBranch({
+					repoName,
+					repoOwnerName,
+					branchName: selectedBranchName,
+				});
+				if (!isBranchValid) {
+					console.log(chalk.red("The given branch name doesn't exist"));
+					throw new Error("The given branch name doesn't exist");
+				}
+			}
+			return {
+				repoOwnerName: repoOwnerName,
+				repoName: repoName,
+				branch: selectedBranchName,
+				filePath: filePath,
+			};
+		}
+		return { repoOwnerName: '', repoName: '', branch: '', filePath: '' };
+	} catch (e) {
+		throw e;
+	}
+};
+
 export const getRepository = async ({
 	exampleName,
 	githubLink,
@@ -30,86 +89,26 @@ export const getRepository = async ({
 				})
 			)
 		);
-
-		if (!exampleName && !githubLink) {
-			const resolvedProjectPath = await createDirectory(projectName, directoryPath);
-			const examples = await getExamplesList();
-			const selectedExampleName = await inquirer.prompt({
-				name: 'selectExample',
-				type: 'list',
-				message: 'Which example would you like to start with?',
-				choices: [...examples],
-			});
-			await retry(
-				() =>
-					downloadAndExtractRepo({
-						root: resolvedProjectPath,
-						repoOwnerName: 'wundergraph',
-						repoName: 'wundergraph',
-						branch: 'main',
-						filePath: `examples/${selectedExampleName['selectExample']}`,
-					}),
-				{
-					retries: 3,
-				}
-			);
-			return 'success';
+		const resolvedProjectPath = await createDirectory(projectName, directoryPath);
+		const { repoOwnerName, repoName, branch, filePath } = await resolveRepository({ exampleName, githubLink });
+		if (repoOwnerName === '' || repoName === '' || branch === '') {
+			console.log(chalk.red('Could not resolve the repository details. Please try again.'));
+			throw new Error('Could not resolve the repository details. Please try again.');
 		}
-		if (exampleName) {
-			const selectedExampleName = await checkIfValidExample(exampleName);
-			const resolvedProjectPath = await createDirectory(projectName, directoryPath);
-			await retry(
-				() =>
-					downloadAndExtractRepo({
-						root: resolvedProjectPath,
-						repoOwnerName: 'wundergraph',
-						repoName: 'wundergraph',
-						branch: 'main',
-						filePath: `examples/${selectedExampleName}`,
-					}),
-				{
-					retries: 3,
-				}
-			);
-			return 'success';
-		} else if (githubLink) {
-			const { repoName, repoOwnerName, branch, filePath } = await getRepoInfo(githubLink);
-			const resolvedProjectPath = await createDirectory(projectName, directoryPath);
-			let selectedBranchName = branch;
-			if (branch === '') {
-				const branchPrompt = await inquirer.prompt({
-					name: 'branch',
-					type: 'input',
-					message: 'Which branch of the repository would you like to use?',
-					default: 'main',
-				});
-				selectedBranchName = branchPrompt['branch'];
-				const isBranchValid = await validateBranch({
-					repoName,
-					repoOwnerName,
-					branchName: selectedBranchName,
-				});
-				if (!isBranchValid) {
-					console.log(chalk.red("The given branch name doesn't exist"));
-					throw new Error("The given branch name doesn't exist");
-				}
+		await retry(
+			() =>
+				downloadAndExtractRepo({
+					root: resolvedProjectPath,
+					repoOwnerName: repoOwnerName,
+					repoName: repoName,
+					branch: branch,
+					filePath: filePath,
+				}),
+			{
+				retries: 3,
 			}
-			await retry(
-				() =>
-					downloadAndExtractRepo({
-						root: resolvedProjectPath,
-						repoOwnerName: repoOwnerName,
-						repoName: repoName,
-						branch: selectedBranchName,
-						filePath: filePath,
-					}),
-				{
-					retries: 3,
-				}
-			);
-			return 'success';
-		}
-		return '';
+		);
+		return 'success';
 	} catch (e) {
 		throw e;
 	}
