b8ad0d1f87908bddb5a6607ce1a4c7ab18aa3d8f
Eelco Wiersma <contact@pagebakers.nl>
2022-11-14 15:05:56 +0100

    chore: remove unused hooks

--- testapps/nextjs/lib/wundergraph.ts
+++
@@ -1,9 +0,0 @@
-import { createWunderGraphNext } from '../components/generated/nextjs';
-
-const wg = createWunderGraphNext({
-	ssr: true,
-});
-
-const { client, withWunderGraph, useQuery, useMutation, useSubscription, useUser, useAuth, useFileUpload } = wg;
-
-export { client, withWunderGraph, useQuery, useMutation, useSubscription, useUser, useAuth, useFileUpload };
