d96acb338420d453f04cd0c59962461c6586678c
“Kaleemullah” <nixamani5@gmail.com>
2022-11-26 05:20:13 +0500

    chore: remove graphqlServers

--- examples/nextjs-todos/.wundergraph/wundergraph.server.ts
+++ examples/nextjs-todos/.wundergraph/wundergraph.server.ts
@@ -1,4 +1,3 @@
-import { GraphQLObjectType, GraphQLSchema, GraphQLString } from 'graphql';
 import { configureWunderGraphServer } from '@wundergraph/sdk';
 import type { HooksConfig } from './generated/wundergraph.hooks';
 import type { InternalClient } from './generated/wundergraph.internal.client';
@@ -8,23 +7,4 @@ export default configureWunderGraphServer<HooksConfig, InternalClient>(() => ({
 		queries: {},
 		mutations: {},
 	},
-	graphqlServers: [
-		{
-			serverName: 'gql',
-			apiNamespace: 'gql',
-			schema: new GraphQLSchema({
-				query: new GraphQLObjectType({
-					name: 'RootQueryType',
-					fields: {
-						hello: {
-							type: GraphQLString,
-							resolve() {
-								return 'world';
-							},
-						},
-					},
-				}),
-			}),
-		},
-	],
 }));
