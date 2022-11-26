31240896b925ebaeb9d77a98ae94aa2a89232c9e
“Kaleemullah” <nixamani5@gmail.com>
2022-11-26 05:45:38 +0500

    chore: delete comments in seed

--- examples/nextjs-todos/seed/seed.ts
+++ examples/nextjs-todos/seed/seed.ts
@@ -1,31 +1,4 @@
-// import { createClient } from '../components/generated/client';
 import fetch from 'node-fetch';
-
-const seed = async () => {
-	// const client = createClient({
-	// 	customFetch: fetch as any,
-	// });
-	// const user = await client.query({
-	// 	operationName: 'UserByEmail',
-	// 	input: {
-	// 		email: 'jens@wundergraph.com',
-	// 	},
-	// });
-	// if (user?.data?.db_findFirstUser) {
-	// 	return;
-	// }
-	// const out = await client.mutate({
-	// 	operationName: 'CreateUser',
-	// 	input: {
-	// 		name: 'Jens',
-	// 		bio: 'Founder@WunderGraph',
-	// 		email: 'jens@wundergraph.com',
-	// 		title: 'Welcome to WunderGraph!',
-	// 		content: 'This is WunderGraph =)',
-	// 		published: true,
-	// 	},
-	// });
-	// console.log('seed:out', JSON.stringify(out));
-};
+const seed = async () => {};
 
 seed();
