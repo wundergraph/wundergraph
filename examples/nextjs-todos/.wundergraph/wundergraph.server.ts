7e59c6cd86f1ac329b9a74ed9848ffa76c6807e7
“Kaleemullah” <nixamani5@gmail.com>
2022-11-28 19:54:51 +0500

    chore: replace single quote to double quote

--- examples/nextjs-todos/.wundergraph/wundergraph.server.ts
+++ examples/nextjs-todos/.wundergraph/wundergraph.server.ts
@@ -1,6 +1,6 @@
-import { configureWunderGraphServer } from '@wundergraph/sdk';
-import type { HooksConfig } from './generated/wundergraph.hooks';
-import type { InternalClient } from './generated/wundergraph.internal.client';
+import { configureWunderGraphServer } from "@wundergraph/sdk";
+import type { HooksConfig } from "./generated/wundergraph.hooks";
+import type { InternalClient } from "./generated/wundergraph.internal.client";
 
 export default configureWunderGraphServer<HooksConfig, InternalClient>(() => ({
 	hooks: {
--- examples/nextjs-todos/tailwind.config.js
+++ examples/nextjs-todos/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import("tailwindcss").Config} */
 module.exports = {
-	content: ['./pages/*.tsx'],
+	content: ["./pages/*.tsx"],
 	theme: {
 		extend: {},
 	},
