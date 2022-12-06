2122639dae15e5b68facc6de1c2b1ce9bfa6d97c
Alberto García Hierro <alberto@garciahierro.com>
2022-12-06 21:34:53 +0000

    feat: add tests for openapi-mocking example using ava

---
+++ examples/openapi-mocking/test.ts
@@ -0,0 +1,49 @@
+import test from 'ava';
+
+import { Server } from '@wundergraph/sdk/testing';
+
+import { createClient } from './.wundergraph/generated/client';
+
+const wg = new Server({ createClient });
+
+test.before(async (t) => {
+	await wg.spinUp();
+});
+
+test.after(async (t) => {
+	await wg.tearDown();
+});
+
+test('create a note', async (t) => {
+	const result = await wg.client().mutate({
+		operationName: 'NewNote',
+		input: {
+			text: 'my first note',
+		},
+	});
+	t.falsy(result.error);
+	t.not(result.data?.notes_newNote?.id ?? 0, 0, 'new note id should not be zero');
+});
+
+test('create a note, read it back', async (t) => {
+	const text = 'not my first note anymore';
+	const mutateResult = await wg.client().mutate({
+		operationName: 'NewNote',
+		input: {
+			text: text,
+		},
+	});
+	t.falsy(mutateResult.error);
+	const id = mutateResult.data?.notes_newNote?.id ?? 0;
+
+	const queryResult = await wg.client().query({
+		operationName: 'NoteByID',
+		input: {
+			id: id,
+		},
+	});
+
+	t.falsy(queryResult.error);
+	t.is(queryResult.data?.notes_noteByID?.id ?? 0, id);
+	t.is(queryResult.data?.notes_noteByID?.text ?? '', text);
+});
