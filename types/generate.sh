c57376d620eb55b583f7a2636cc3326bb7ad6e3d
Alberto García Hierro <alberto@garciahierro.com>
2022-12-08 11:39:36 +0000

    chore: remove version numbers from generated PB Go file
    Although version numbers could provide some value to detect version
    incompatibility, we're only using PB to generate schema definitions
    for JSON files, so this is a minor concern. Additionally, we do not
    have version numbers in the TS side (because the generator doesn't
    write them down). On the other hand, including the version number
    causes significant friction for contributors using slightly different
    PB versions while checking that the generated files are up to date)

--- types/generate.sh
+++ types/generate.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env sh
 
+set -x
+set -e
+
 protoc  \
   --proto_path=./protos \
   --go_out=.. \
@@ -7,3 +10,10 @@ protoc  \
   --go_opt=paths=import \
   --experimental_allow_proto3_optional \
   wundernode_config.proto
+
+outfile='../pkg/wgpb/wundernode_config.pb.go'
+
+# Remove version numbers. Generating the backup is required
+# for compatibility with both GNU and BSD sed variants.
+sed -i.bak '2,4d' ${outfile}
+rm -f ${outfile}.bak
