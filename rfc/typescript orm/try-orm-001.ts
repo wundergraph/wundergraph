634ff2611e8cd9743cc7150788628fad3545625e
Jens Neuse <jens.neuse@gmx.de>
2022-12-09 11:35:50 +0100

    chore: try orm

--- rfc/typescript orm/try-orm-001.ts
+++ rfc/typescript orm/try-orm-001.ts
@@ -11,12 +11,12 @@ const userAccount = tsql.query({
 		id: tsql.string,
 		first: tsql.number,
 	},
-	query: ({ args: { id }, query }) =>
+	query: ({ args: { id, first }, query }) =>
 		query.user({ id }, (u) => {
 			u.id();
 			u.name();
 			u.email();
-			postsFragment(u, { first: 10 });
+			postsFragment(u, { first });
 		}),
 });
 
@@ -39,3 +39,6 @@ const data = await userAccount({
 
 console.log(data.user.id); // 1
 console.log(data.user.name); // 'John Doe'
+console.log(data.user.posts[0].id); // 1
+console.log(data.user.posts[0].title); // 'Hello World'
+console.log(data.user.posts[0].content); // 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
