6a05add61e0c6c68edc9c47dfddda13c58508156
starptech <deusdustin@gmail.com>
2022-12-10 13:55:06 +0100

    chore: improve

--- pkg/telemetry/metrics.go
+++ pkg/telemetry/metrics.go
@@ -6,11 +6,11 @@ import (
 )
 
 func CmdUsageMetricName(cmdName string) string {
-	return fmt.Sprintf("WUNDERCTL_%s_CMD_USAGE", strings.ToUpper(cmdName))
+	return fmt.Sprintf("%s_CMD_USAGE", strings.ToUpper(cmdName))
 }
 
 func CmdDurationMetricName(cmdName string) string {
-	return fmt.Sprintf("WUNDERCTL_%s_CMD_DURATION", strings.ToUpper(cmdName))
+	return fmt.Sprintf("%s_CMD_DURATION", strings.ToUpper(cmdName))
 }
 
 func CmdMetricNameWithParent(parentCmdName, cmdName string) string {
