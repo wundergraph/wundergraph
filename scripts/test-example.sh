6272d9238d2f90b9ff89cb5d4e7ac5fe8bcbabeb
Alberto García Hierro <alberto@garciahierro.com>
2022-12-10 16:24:24 +0000

    feat(ci): add support for examples with docker compose clusters

--- scripts/test-example.sh
+++ scripts/test-example.sh
@@ -12,15 +12,26 @@ set -e
 grep '@wundergraph' package.json | awk '{gsub(/"|:/, "", $1); print $1"@workspace"}'| xargs pnpm install
 pnpm install --no-frozen-lockfile
 
+
+# If we have a Docker cluster, bring it up
+if test -f docker-compose.yml; then
+    docker-compose up -d
+    sleep 5
+fi
+
 # Generate WunderGraph files
 wunderctl generate
 
-
-
+# Run test if available, otherwise just type-check
 if grep '"test"' package.json > /dev/null; then
     pnpm test
 else
     pnpm check
+fi
+
+# If we have a Docker cluster, clean it up
+if test -f docker-compose.yml; then
+    docker-compose down
 fi
 
 # Restore package.json
