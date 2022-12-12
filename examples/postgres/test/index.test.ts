e0365daf6341c6c073004934dd23ebbadbaa152b
Suvij Surya <suvijsurya76@gmail.com>
2022-12-12 18:04:54 +0530

    feat: add tests for postgres example

---
+++ examples/postgres/test/index.test.ts
@@ -0,0 +1,28 @@
+import { afterAll, beforeAll, describe, expect, test } from '@jest/globals';
+import fetch from 'node-fetch';
+import { createTestServer } from '../.wundergraph/generated/testing';
+
+const wg = createTestServer({ fetch: fetch as any });
+
+beforeAll(async () => {
+	await wg.start();
+});
+
+afterAll(async () => {
+	await wg.stop();
+});
+
+describe('Test users and messages API', () => {
+	test('users', async () => {
+		const result = await wg.client().query({
+			operationName: 'Users',
+		});
+		expect(result.data?.db_findFirstusers.name).toBe('Jens@WunderGraph');
+	});
+	test('messages', async () => {
+		const result = await wg.client().query({
+			operationName: 'Messages',
+		});
+		expect(result.data?.db_findManymessages.length).toBe(3);
+	});
+});
