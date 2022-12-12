c4c6ef2d08b3dc8f9060382611f62e3b11f7e8a8
Suvij Surya <suvijsurya76@gmail.com>
2022-12-12 17:08:31 +0530

    feat: add tests for fragments example

---
+++ examples/fragments/test/index.test.ts
@@ -0,0 +1,28 @@
+import { afterAll, beforeAll, describe, expect, test } from '@jest/globals';
+import fetch from 'node-fetch';
+import { createTestServer } from '../.wundergraph/generated/testing';
+
+const wg = createTestServer({ fetch: fetch as any });
+
+beforeAll(async () => {
+	await wg.start();
+});
+
+afterAll(async () => {
+	await wg.stop();
+});
+
+describe('Test Fragments', () => {
+	test('local fragment', async () => {
+		const result = await wg.client().query({
+			operationName: 'LocalFragment',
+		});
+		expect(result.data?.spacex_dragons.length).toBe(2);
+	});
+	test('global fragment', async () => {
+		const result = await wg.client().query({
+			operationName: 'GlobalFragment',
+		});
+		expect(result.data?.spacex_dragons.length).toBe(10);
+	});
+});
