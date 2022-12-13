0c3ab748fcd2d1d3fb9fa1cbdb87df3f053a0d7d
Alberto García Hierro <alberto@garciahierro.com>
2022-12-13 12:45:04 +0000

    fix: return a 400 bad request when POST'ed JSON is invalid (#416)
    * fix: return a 400 bad request when POST'ed JSON is invalid
    
    Response was failing to serialize because we were trying to include the
    JSON back as a json.RawMessage, which failed to serialize. To avoid silently
    ignoring another problem here in the future, make sure any errors writing
    the error to the output are propagated to the caller.
    
    * refactor: set HTTP response code for validation error from caller
    
    Pass a buffer to validator.Validate() and if the result turns out to
    be invalid, set the response code to 400 and copy the buffer to the
    http.ResponseWriter
    
    * chore: make JSON field names in input validation errors PascalCase
    
    * chore: make JSON field names in input validation errors camelCase
    
    Also, fix a warning in the tests
    
    * fix: test against fixtures in pkg/node that changed due to the API change

--- pkg/apihandler/apihandler.go
+++ pkg/apihandler/apihandler.go
@@ -1079,8 +1079,7 @@ func (h *QueryHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	ctx.Variables = parseQueryVariables(r, h.queryParamsAllowList)
 	ctx.Variables = h.stringInterpolator.Interpolate(ctx.Variables)
 
-	valid := h.variablesValidator.Validate(ctx, ctx.Variables, inputvariables.NewValidationWriter(w))
-	if !valid {
+	if !validateInputVariables(requestLogger, ctx, h.variablesValidator, w) {
 		return
 	}
 
@@ -1514,8 +1513,8 @@ func (h *MutationHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		ctx.Variables = []byte("{}")
 	}
 	ctx.Variables = h.stringInterpolator.Interpolate(ctx.Variables)
-	valid := h.variablesValidator.Validate(ctx, ctx.Variables, inputvariables.NewValidationWriter(w))
-	if !valid {
+
+	if !validateInputVariables(requestLogger, ctx, h.variablesValidator, w) {
 		return
 	}
 
@@ -1663,8 +1662,8 @@ func (h *SubscriptionHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) 
 
 	ctx.Variables = parseQueryVariables(r, h.queryParamsAllowList)
 	ctx.Variables = h.stringInterpolator.Interpolate(ctx.Variables)
-	valid := h.variablesValidator.Validate(ctx, ctx.Variables, inputvariables.NewValidationWriter(w))
-	if !valid {
+
+	if !validateInputVariables(requestLogger, ctx, h.variablesValidator, w) {
 		return
 	}
 
@@ -2325,5 +2324,23 @@ func handleOperationErr(log *zap.Logger, err error, w http.ResponseWriter, error
 		zap.Error(err),
 	)
 	http.Error(w, errorMessage, http.StatusInternalServerError)
+	return true
+}
+
+func validateInputVariables(log *zap.Logger, ctx *resolve.Context, validator *inputvariables.Validator, w http.ResponseWriter) bool {
+	var buf bytes.Buffer
+	valid, err := validator.Validate(ctx, ctx.Variables, &buf)
+	if err != nil {
+		log.Error("failed to validate input variables", zap.Error(err))
+		w.WriteHeader(http.StatusInternalServerError)
+		return false
+	}
+	if !valid {
+		w.WriteHeader(http.StatusBadRequest)
+		if _, err := io.Copy(w, &buf); err != nil {
+			log.Error("copying validation to response", zap.Error(err))
+		}
+		return false
+	}
 	return true
 }
