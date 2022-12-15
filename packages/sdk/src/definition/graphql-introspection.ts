79dde43d39715ad2b8c532fee69ed2411bde28b6
Alberto García Hierro <alberto@garciahierro.com>
2022-12-15 12:47:33 +0000

    chore: reword error messages, introduce custom Error type

--- packages/sdk/src/definition/graphql-introspection.ts
+++ packages/sdk/src/definition/graphql-introspection.ts
@@ -35,6 +35,13 @@ import { Logger } from '../logger';
 import { mergeSchemas } from '@graphql-tools/schema';
 import transformSchema from '../transformations/shema';
 
+class MissingKeyError extends Error {
+	constructor(private key: string, private introspection: GraphQLIntrospection) {
+		super(`${key} is not defined in your ${introspection.apiNamespace} datasource`);
+		Object.setPrototypeOf(this, MissingKeyError.prototype);
+	}
+}
+
 export const resolveGraphqlIntrospectionHeaders = (headers?: { [key: string]: HTTPHeader }): Record<string, string> => {
 	const baseHeaders: Record<string, string> = {
 		'Content-Type': 'application/json',
@@ -89,7 +96,7 @@ export const introspectGraphql = async (introspection: GraphQLIntrospection): Pr
 				serviceSDL = loadFile(introspection.loadSchemaFromString);
 			} else {
 				if (!introspection.url) {
-					throw new Error(`introspection URL is not defined for API ${introspection.apiNamespace}`);
+					throw new MissingKeyError('url', introspection);
 				}
 				serviceSDL = await fetchFederationServiceSDL(resolveVariable(introspection.url), introspectionHeaders, {
 					apiNamespace: introspection.apiNamespace,
@@ -222,10 +229,10 @@ const introspectGraphQLAPI = async (
 
 	if (introspection.mTLS) {
 		if (!introspection.mTLS.key) {
-			throw new Error(`introspection mTLS key is not defined in API ${introspection.apiNamespace}`);
+			throw new MissingKeyError('mTLS.key', introspection);
 		}
 		if (!introspection.mTLS.cert) {
-			throw new Error(`introspection mTLS cert is not defined in API ${introspection.apiNamespace}`);
+			throw new MissingKeyError('mTLS.cert', introspection);
 		}
 		opts.httpsAgent = new https.Agent({
 			key: resolveVariable(introspection.mTLS.key),
@@ -237,7 +244,7 @@ const introspectGraphQLAPI = async (
 	let res: AxiosResponse | undefined;
 	try {
 		if (!introspection.url) {
-			throw new Error(`introspection URL is not defined for API ${introspection.apiNamespace}`);
+			throw new MissingKeyError('url', introspection);
 		}
 		res = await Fetcher().post(resolveVariable(introspection.url), data, opts);
 	} catch (e: any) {
