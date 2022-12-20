5e857cd4416c031ad670b358a00dc3f4f7ef4f85
Alberto García Hierro <alberto@garciahierro.com>
2022-12-20 16:01:44 +0000

    fix: minio setup script required a real tty, which made it fail in CI

--- examples/nextjs-file-upload/minio/setup.sh
+++ examples/nextjs-file-upload/minio/setup.sh
@@ -6,12 +6,12 @@ docker-compose up -d
 sleep 3
 
 function setupMinio () {
-    docker-compose exec minio mc alias set myminio/ http://127.0.0.1:9000 minio minio123
-    docker-compose exec minio mc admin user add myminio test 12345678
-    docker-compose exec minio mc admin policy set myminio readwrite user=test
-    docker-compose exec minio mc admin user info myminio test
-    docker-compose exec minio mc mb myminio/uploads
-    docker-compose exec minio mc policy set public myminio/uploads
+    docker-compose exec -T minio mc alias set myminio/ http://127.0.0.1:9000 minio minio123
+    docker-compose exec -T minio mc admin user add myminio test 12345678
+    docker-compose exec -T minio mc admin policy set myminio readwrite user=test
+    docker-compose exec -T minio mc admin user info myminio test
+    docker-compose exec -T minio mc mb myminio/uploads
+    docker-compose exec -T minio mc policy set public myminio/uploads
 	return $?
 }
 
