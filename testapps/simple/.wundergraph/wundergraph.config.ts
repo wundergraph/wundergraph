bf813b9a756d6abce88866a7993e5b86f6b8144e
Sergey Petrunin <neyasut@gmail.com>
2022-12-29 16:01:21 +0200

    chore: add simple basic test app

---
+++ testapps/simple/.wundergraph/wundergraph.config.ts
@@ -0,0 +1,36 @@
+import { configureWunderGraphApplication, cors, EnvironmentVariable, introspect, templates } from '@wundergraph/sdk';
+import server from './wundergraph.server';
+import operations from './wundergraph.operations';
+
+// configureWunderGraph emits the configuration
+configureWunderGraphApplication({
+	apis: [],
+	server,
+	operations,
+	codeGenerators: [
+		{
+			templates: [
+				// use all the typescript react templates to generate a client
+				...templates.typescript.all,
+			],
+			// create-react-app expects all code to be inside /src
+			// path: "../frontend/src/generated",
+		},
+	],
+	cors: {
+		...cors.allowAll,
+		allowedOrigins:
+			process.env.NODE_ENV === 'production'
+				? [
+						// change this before deploying to production to the actual domain where you're deploying your app
+						'http://localhost:3000',
+				  ]
+				: ['http://localhost:3000', new EnvironmentVariable('WG_ALLOWED_ORIGIN')],
+	},
+	dotGraphQLConfig: {
+		hasDotWunderGraphDirectory: false,
+	},
+	security: {
+		enableGraphQLEndpoint: process.env.NODE_ENV !== 'production' || process.env.GITPOD_WORKSPACE_ID !== undefined,
+	},
+});
