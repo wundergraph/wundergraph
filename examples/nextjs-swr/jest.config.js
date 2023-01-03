5b571aaf5fa5e9a9357b45c169109cf0291ff726
Eelco Wiersma <contact@pagebakers.nl>
2023-01-03 11:27:15 +0000

    chore: fix nextjs-swr example (#510)

--- examples/nextjs-swr/jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-	roots: ['<rootDir>/test/'],
-	transform: {
-		'^.+\\.tsx?$': 'ts-jest',
-	},
-	testRegex: '(/__tests__/.*|\\.(test|spec))\\.[tj]sx?$',
-	moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-};
