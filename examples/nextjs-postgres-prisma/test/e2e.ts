2e81fed2ab1b6098757ba6092a5f90666bbb2e81
Alberto García Hierro <alberto@garciahierro.com>
2023-01-06 15:20:33 +0000

    chore: remove changes outside nextjs example to facilitate review

--- examples/nextjs-postgres-prisma/test/e2e.ts
+++
@@ -1,9 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('page displays expected results', async ({ page }) => {
-	await page.goto('/');
-
-	const result = page.locator('#result');
-
-	expect((await result.textContent()) ?? '').toContain('Founder@WunderGraph');
-});
--- examples/nextjs-react-query/pages/index.tsx
+++ examples/nextjs-react-query/pages/index.tsx
@@ -7,7 +7,6 @@ const Home: NextPage = () => {
 	});
 
 	return (
-<<<<<<< HEAD
 		<div>
 			<div className="relative max-w-5xl mx-auto pt-20 sm:pt-24 lg:pt-28">
 				<div className="flex justify-center">
@@ -34,54 +33,6 @@ const Home: NextPage = () => {
 	c0-30.5,5.8-59.7,16.7-86.5L484.4,669l69-116.7l68.5,116.5l83.8-142.5H785C772,642.8,673.3,733.2,553.4,733.2z"
 							/>
 						</svg>
-=======
-		<div className={'w-full h-screen'}>
-			<Nav />
-			<div className="relative max-w-5xl mx-auto pt-20 sm:pt-24 lg:pt-32">
-				<div className={'flex flex-col items-center h-screen'}>
-					<h1 className={'mb-4 text-4xl font-extrabold md:text-5xl lg:text-5xl'}>
-						{' '}
-						Welcome to{' '}
-						<span className="inline-block text-transparent bg-clip-text bg-gradient-to-r from-red-500 to-amber-500">
-							{' '}
-							React Query
-						</span>{' '}
-						with
-					</h1>
-					<h1 className={'mb-4 text-4xl font-extrabold md:text-5xl lg:text-5xl'}>
-						<span className="bg-gradient-to-r from-purple-400 to-pink-600 bg-clip-text text-transparent">
-							{' '}
-							WunderGraph
-						</span>
-					</h1>
-					<div className="relative flex flex-col items-center overflow-hidden p-8 sm:p-12">
-						<div className="w-full max-w-xl rounded-2xl bg-blue-50 px-20 py-14">
-							<div className="mx-auto flex max-w-sm flex-col items-center">
-								<p className="mt-3 mb-8 text-center text-black/80">
-									This is the result of your{' '}
-									<code className="font-mono font-medium text-amber-500 font-bold">Dragons</code> operation.
-								</p>
-								<ul id="result">
-									{dragons.data?.spacex_dragons?.map((dragon) => {
-										return <li key={dragon.id}>{JSON.stringify(dragon)}</li>;
-									})}
-								</ul>
-							</div>
-						</div>
-						<footer className="flex justify-between text-gray-400">
-							<p className="pt-3">
-								Visit{' '}
-								<a
-									className="text-cyan-400 hover:text-cyan-600"
-									target="_blank"
-									href="https://github.com/wundergraph/wundergraph"
-								>
-									Github
-								</a>{' '}
-								to learn more about WunderGraph.
-							</p>
-						</footer>
->>>>>>> 443daec3 (test: add e2e tests to nextjs-react-query example)
 					</div>
 				</div>
 				<h1 className="text-slate-900 font-extrabold text-4xl sm:text-5xl lg:text-6xl tracking-tight text-center dark:text-white">
