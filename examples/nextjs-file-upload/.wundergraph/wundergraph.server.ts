1d571c80ff4de754cf244e6348226c9eb04b9fa5
Alberto García Hierro <alberto@garciahierro.com>
2023-01-12 10:33:27 +0000

    fix: add missing files to nextjs-file-upload example

---
+++ examples/nextjs-file-upload/.wundergraph/wundergraph.server.ts
@@ -0,0 +1,49 @@
+import { GraphQLObjectType, GraphQLSchema, GraphQLString } from 'graphql';
+import { configureWunderGraphServer } from '@wundergraph/sdk/server';
+import type { HooksConfig } from './generated/wundergraph.hooks';
+import type { InternalClient } from './generated/wundergraph.internal.client';
+
+export default configureWunderGraphServer<HooksConfig, InternalClient>(() => ({
+	hooks: {
+		queries: {},
+		mutations: {},
+		uploads: {
+			minio: {
+				coverPicture: {
+					preUpload: ({ user, file, meta }) => {
+						console.log(`preUpload user: ${user}, file: ${file}, meta: ${meta}`);
+						if (!user) {
+							return { error: 'authenticate' };
+						}
+						return { fileKey: 'myfile.png' };
+						//						return {fileKey: file.name}
+					},
+					postUpload: async ({ user, file, meta, internalClient, error }) => {
+						console.log(
+							`postUpload user: ${user}, file: ${file}, meta: ${meta}, internalClient: ${internalClient}, error: ${error}`
+						);
+					},
+				},
+			},
+		},
+	},
+	graphqlServers: [
+		{
+			serverName: 'gql',
+			apiNamespace: 'gql',
+			schema: new GraphQLSchema({
+				query: new GraphQLObjectType({
+					name: 'RootQueryType',
+					fields: {
+						hello: {
+							type: GraphQLString,
+							resolve() {
+								return 'world';
+							},
+						},
+					},
+				}),
+			}),
+		},
+	],
+}));
