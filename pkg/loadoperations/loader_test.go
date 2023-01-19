0d828c770f66479876fca0c3a3e38f0a7e81bb01
Sergey Petrunin <neyasut@gmail.com>
2023-01-19 18:16:22 +0200

    fix: naming of loaded operations, refactor loader

--- pkg/loadoperations/loader_test.go
+++ pkg/loadoperations/loader_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/wundergraph/graphql-go-tools/pkg/testing/goldie"
 )
 
 func TestIsValidOperationName(t *testing.T) {
@@ -14,4 +15,12 @@ func TestIsValidOperationName(t *testing.T) {
 	assert.False(t, isValidOperationName("2Foo"))
 	assert.False(t, isValidOperationName("-Foo"))
 	assert.False(t, isValidOperationName("Foo-"))
+}
+
+func TestLoader_Load(t *testing.T) {
+	loader := NewLoader("testdata/operations", "testdata/fragments", "testdata/schema.graphql")
+	out, err := loader.Load(true)
+	assert.NoError(t, err)
+
+	goldie.New(t).Assert(t, "loadoperations", []byte(out))
 }
