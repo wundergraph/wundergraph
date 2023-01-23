c108525b7652907b6146846d6cb16fbf0f2dce01
Eelco Wiersma <contact@pagebakers.nl>
2023-01-23 15:55:22 +0000

    chore: fix import

--- docs-website/src/components/QuickLinks.jsx
+++ docs-website/src/components/QuickLinks.jsx
@@ -1,5 +1,5 @@
 import Link from 'next/link'
-import { clsx } from 'clsx'
+import clsx from 'clsx'
 import { Icon } from '@/components/Icon'
 import { ArrowRightIcon } from '@heroicons/react/24/solid'
 
