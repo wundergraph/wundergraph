75586c2a64b54e0b569bd2183e4df10ba62ba621
Jens Neuse <jens.neuse@gmx.de>
2023-01-26 11:34:26 +0100

    feat: cleanup introspection, enable introspecting large database schemas

--- cli/commands/mongodb.go
+++ cli/commands/mongodb.go
@@ -4,9 +4,6 @@ import (
 	"fmt"
 
 	"github.com/spf13/cobra"
-
-	"github.com/wundergraph/wundergraph/pkg/datasources/database"
-	"github.com/wundergraph/wundergraph/pkg/files"
 )
 
 // postgresCmd represents the postgres command
@@ -16,27 +13,13 @@ var mongoDbCmd = &cobra.Command{
 	Example: `wunderctl mongodb mongodb+srv://test:test@cluster0.ns1yp.mongodb.net/myFirstDatabase`,
 	Args:    cobra.ExactArgs(1),
 	RunE: func(cmd *cobra.Command, args []string) error {
-		wunderGraphDir, err := files.FindWunderGraphDir(_wunderGraphDirConfig)
-		if err != nil {
-			return err
-		}
 		databaseURL := args[0]
 		introspectionSchema := fmt.Sprintf(`datasource db {
   provider = "mongodb"
   url      = "%s"
 }
 `, databaseURL)
-		prismaSchema, graphqlSDL, dmmf, err := database.IntrospectPrismaDatabase(introspectionSchema, wunderGraphDir, log)
-		if err != nil {
-			return err
-		}
-		result := DatabaseIntrospectionResult{
-			PrismaSchema:  prismaSchema,
-			GraphQLSchema: graphqlSDL,
-			Dmmf:          []byte(dmmf),
-		}
-		emitIntrospectionResult(result)
-		return nil
+		return introspectDatabase(introspectionSchema)
 	},
 }
 
