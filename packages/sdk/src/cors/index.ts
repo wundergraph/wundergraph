856e31e54db3cb61b0be9fa23d1c6b42310a1a92
JivusAyrus <suvijsurya76@gmail.com>
2023-02-10 20:08:36 +0530

    chore: add comments to mention the reason for not assigning the allowed origins as *

--- packages/sdk/src/cors/index.ts
+++ packages/sdk/src/cors/index.ts
@@ -4,6 +4,7 @@ const allowAll: WunderGraphCorsConfiguration = {
 	allowCredentials: true,
 	allowedHeaders: ['*'],
 	allowedMethods: ['GET', 'POST'],
+	// could not make the allowed origins as '*', because on the client we use credentials: include
 	allowedOrigins: ['http://*', 'https://*'],
 	exposedHeaders: ['*'],
 	maxAge: 120,
