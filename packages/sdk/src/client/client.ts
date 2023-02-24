4fd75f192c1a6de84116c94f5887e8cf39fae539
StarpTech <deusdustin@gmail.com>
2023-02-24 16:16:03 +0100

    fix: deep clone document before applying patch to avoid cache hit by reference

--- packages/sdk/src/client/client.ts
+++ packages/sdk/src/client/client.ts
@@ -375,14 +375,15 @@ export class Client {
 				if (lastResponse !== null && Array.isArray(jsonResp)) {
 					// we have a lastResponse and the current response is a json patch
 					// we apply the patch to generate the latest response
-					lastResponse = applyPatch(lastResponse, jsonResp).newDocument as GraphQLResponse;
+					// applyPatch deep clones the document before applying the patch
+					// in that way we always ensure that the response is not cached by reference by clients / caches
+					// e.g. react works with reference equality to determine if a component needs to be re-rendered
+					lastResponse = applyPatch(lastResponse, jsonResp, true, false, true).newDocument as GraphQLResponse;
 				} else {
 					// it's not a patch, so we just set the lastResponse to the current response
 					lastResponse = jsonResp as GraphQLResponse;
 				}
-				console.log(`event ${JSON.stringify(lastResponse)}`);
 				const clientResponse = this.convertGraphQLResponse(lastResponse);
-				console.log(`clientResponse ${JSON.stringify(clientResponse)}`);
 				cb(clientResponse);
 			});
 			if (subscription?.abortSignal) {
