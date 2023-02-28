a69d54be43883cc18a42f8fc5621d8f808bb1b54
Aenimus <47415099+Aenimus@users.noreply.github.com>
2023-02-28 17:37:34 +0000

    feat: store generated authentication secrets in secrets manager on deploy (#666)
    Co-authored-by: David Stutt <david@wundergraph.com>

--- examples/nextjs-typescript-functions/.wundergraph/wundergraph.config.ts
+++ examples/nextjs-typescript-functions/.wundergraph/wundergraph.config.ts
@@ -1,11 +1,4 @@
-import {
-	authProviders,
-	configureWunderGraphApplication,
-	cors,
-	EnvironmentVariable,
-	introspect,
-	templates,
-} from '@wundergraph/sdk';
+import { authProviders, configureWunderGraphApplication, cors, introspect, templates } from '@wundergraph/sdk';
 import { NextJsTemplate } from '@wundergraph/nextjs/dist/template';
 import server from './wundergraph.server';
 import operations from './wundergraph.operations';
@@ -47,15 +40,6 @@ configureWunderGraphApplication({
 		cookieBased: {
 			providers: [authProviders.demo()],
 			authorizedRedirectUris: ['http://localhost:3000'],
-			secureCookieHashKey: new EnvironmentVariable(
-				'WUNDERGRAPH_SECURE_COOKIE_HASH_KEY',
-				'00000000000000000000000000000000'
-			), // must be of length 32
-			secureCookieBlockKey: new EnvironmentVariable(
-				'WUNDERGRAPH_SECURE_COOKIE_BLOCK_KEY',
-				'00000000000000000000000000000000'
-			), // must be of length 32
-			csrfTokenSecret: new EnvironmentVariable('WUNDERGRAPH_CSRF_TOKEN_SECRET', '00000000000'), // must be of length 11
 		},
 	},
 	security: {
