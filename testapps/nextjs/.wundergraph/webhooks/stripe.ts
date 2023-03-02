35e69a3c174ba94189a95428f06946a0f992db31
Eelco Wiersma <contact@pagebakers.nl>
2023-03-02 15:43:48 +0100

    feat: improve webhooks API and add support for the new operations client (#709)
    * feat: add webhook factory
    
    * chore: fix nil exception
    
    * feat: update docs and examples
    
    * chore: update types and fix tests
    
    ---------
    
    Co-authored-by: Jens Neuse <jens.neuse@gmx.de>

--- testapps/nextjs/.wundergraph/webhooks/stripe.ts
+++ testapps/nextjs/.wundergraph/webhooks/stripe.ts
@@ -3,12 +3,19 @@ import Stripe from 'stripe';
 
 import { WebhookHttpEvent, WebhookHttpResponse } from '@wundergraph/sdk/server';
 
-export default createWebhook<WebhookHttpEvent<Stripe.Event>, WebhookHttpResponse<{ type: string }>>({
+export default createWebhook<WebhookHttpEvent<Stripe.Event>, WebhookHttpResponse<{ type: string; name?: string }>>({
 	handler: async (event, ctx) => {
+		const user = await ctx.operations.query({
+			operationName: 'Hello',
+			input: {
+				hello: '1',
+			},
+		});
 		return {
 			statusCode: 200,
 			body: {
 				type: event.body?.type || 'unknown',
+				name: user.data?.gql_hello,
 			},
 		};
 	},
