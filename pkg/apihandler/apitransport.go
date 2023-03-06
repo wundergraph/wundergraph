c71a23193a2c0ba9ce772e9c864562680fc3be8f
Alberto García Hierro <alberto@garciahierro.com>
2023-03-06 14:56:50 +0000

    refactor: make the checks more obvious

--- pkg/apihandler/apitransport.go
+++ pkg/apihandler/apitransport.go
@@ -242,7 +242,7 @@ func (t *ApiTransport) internalGraphQLRoundTrip(request *http.Request) (res *htt
 	}
 
 	// Make sure we account for both pool.ClientRequestKey being nil and being non present
-	if clientRequest, _ := request.Context().Value(pool.ClientRequestKey).(*http.Request); clientRequest != nil {
+	if clientRequest, ok := request.Context().Value(pool.ClientRequestKey).(*http.Request); ok && clientRequest != nil {
 		requestJSON, err := hooks.HttpRequestToWunderGraphRequestJSON(clientRequest, false)
 		if err != nil {
 			return nil, err
