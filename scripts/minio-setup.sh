07f0e01b053bede1727f83325ca8ce981ed8a964
Alberto García Hierro <alberto@garciahierro.com>
2023-03-22 10:37:14 +0000

    fix: argument order to docker exec

--- scripts/minio-setup.sh
+++ scripts/minio-setup.sh
@@ -2,9 +2,9 @@
 
 set -e
 
-docker exec minio -T mc alias set minio http://localhost:9000 minio minio123
-docker exec minio -T mc admin user add minio test 12345678
-docker exec minio -T mc admin policy set minio readwrite user=test
-docker exec minio -T mc admin user info minio test
-docker exec minio -T mc mb --ignore-existing minio/uploads
-docker exec minio -T mc policy set public minio/uploads
+docker exec -T minio mc alias set minio http://localhost:9000 minio minio123
+docker exec -T minio mc admin user add minio test 12345678
+docker exec -T minio mc admin policy set minio readwrite user=test
+docker exec -T minio mc admin user info minio test
+docker exec -T minio mc mb --ignore-existing minio/uploads
+docker exec -T minio mc policy set public minio/uploads
