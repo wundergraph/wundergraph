babbbfc26b9876b1e152b372ffc8ea2a1ee97351
Alberto García Hierro <alberto@garciahierro.com>
2023-03-22 09:25:11 +0000

    chore: use the local node_modules instead of a shared one at the repo top

--- cli/helpers/cache.go
+++ cli/helpers/cache.go
@@ -19,29 +19,10 @@ func GlobalWunderGraphCacheDir() (string, error) {
 // LocalWunderGraphCacheDir returns the absolute path to the
 // cache directory for the current WunderGraph app
 func LocalWunderGraphCacheDir(wgDir string) (string, error) {
-	// Find the top node_modules directory from the WunderGraph
-	// directory by walking up
-	abs, err := filepath.Abs(wgDir)
-	if err != nil {
-		return "", err
+	// Use the local node_modules directory, creating it if it doesn't exist
+	cachePath := filepath.Join("node_modules", ".cache", "wundergraph")
+	if err := os.MkdirAll(cachePath, 0755); err != nil {
+		return "", fmt.Errorf("creating cache directory: %w", err)
 	}
-	const nodeModulesDirName = "node_modules"
-	topNodeModules := ""
-	cur := abs
-	for {
-		nodeModules := filepath.Join(cur, nodeModulesDirName)
-		if st, err := os.Stat(nodeModules); err == nil && st.IsDir() {
-			topNodeModules = nodeModules
-		}
-		next := filepath.Dir(cur)
-		if next == cur {
-			// We're at the root
-			break
-		}
-		cur = next
-	}
-	if topNodeModules != "" {
-		return filepath.Join(topNodeModules, ".cache", "wundergraph"), nil
-	}
-	return "", fmt.Errorf("could not find %s", nodeModulesDirName)
+	return cachePath, nil
 }
