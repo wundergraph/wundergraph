a72f277fa4c8a9e5034d790004b45a01f1a74f1a
Alberto García Hierro <alberto@garciahierro.com>
2023-03-22 11:00:44 +0000

    chore: upgrade minio image in CI

--- scripts/minio-setup.sh
+++ scripts/minio-setup.sh
@@ -1,10 +1,24 @@
 #!/bin/bash
 
 set -e
+set -x
 
-docker exec minio mc alias set minio http://localhost:9000 minio minio123
-docker exec minio mc admin user add minio test 12345678
-docker exec minio mc admin policy set minio readwrite user=test
-docker exec minio mc admin user info minio test
-docker exec minio mc mb --ignore-existing minio/uploads
-docker exec minio mc policy set public minio/uploads
+if ! [[ -x scripts/mc ]]; then
+	if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+		wget https://dl.min.io/client/mc/release/linux-amd64/mc -P scripts/
+	elif [[ "$OSTYPE" == "darwin"* ]]; then
+		if [[ $(uname -p) == "arm" ]]; then
+			curl https://dl.min.io/client/mc/release/darwin-arm64/mc --output scripts/mc
+		elif [[ $(uname -p) == "i386" ]]; then
+			curl https://dl.min.io/client/mc/release/darwin-amd64/mc --output scripts/mc
+		fi
+	fi
+fi
+
+chmod +x ./scripts/mc
+./scripts/mc alias set minio http://localhost:9000 minio minio123
+./scripts/mc admin user add minio test 12345678
+./scripts/mc admin policy attach minio readwrite -u test
+./scripts/mc admin user info minio test
+./scripts/mc mb --ignore-existing minio/uploads
+./scripts/mc policy set public minio/uploads
