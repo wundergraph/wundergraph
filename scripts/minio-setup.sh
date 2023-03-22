1df01809282dbff3942a25a58645219b0ff6dc69
Alberto García Hierro <alberto@garciahierro.com>
2023-03-22 10:11:10 +0000

    fix: use bundled mc for minio setup during CI

--- scripts/minio-setup.sh
+++ scripts/minio-setup.sh
@@ -2,21 +2,9 @@
 
 set -e
 
-if ! [[ -x scripts/mc ]]; then
-	if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-		wget https://dl.min.io/client/mc/release/linux-amd64/mc -P scripts/
-	elif [[ "$OSTYPE" == "darwin"* ]]; then
-		if [[ $(uname -p) == "arm" ]]; then
-			curl https://dl.min.io/client/mc/release/darwin-arm64/mc --output scripts/mc
-		elif [[ $(uname -p) == "i386" ]]; then
-			curl https://dl.min.io/client/mc/release/darwin-amd64/mc --output scripts/mc
-		fi
-	fi
-fi
-
-chmod +x ./scripts/mc
-./scripts/mc alias set minio http://localhost:9000 minio minio123
-./scripts/mc admin user add minio test 12345678
-./scripts/mc admin user info minio test
-./scripts/mc mb --ignore-existing minio/uploads
-./scripts/mc anonymous -r set public minio/uploads
+docker exec minio -T mc alias set minio http://localhost:9000 minio minio123
+docker exec minio -T mc admin user add minio test 12345678
+docker exec minio -T mc admin policy set minio readwrite user=test
+docker exec minio -T mc admin user info minio test
+docker exec minio -T mc mb --ignore-existing minio/uploads
+docker exec minio -T mc policy set public minio/uploads
