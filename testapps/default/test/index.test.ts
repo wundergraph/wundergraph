wundergraph/wundergraph

commit 3734d1d42a978c378dd5d6c9b44ad0e93c5ede30

feat: add support for hooks in internal API calls (#669)

pkg/apihandler/internalapihandler.go
@@ -7,7 +7,6 @@ import (
 	"fmt"
 	"io"
 	"net/http"
-	"sync"
 
 	"github.com/buger/jsonparser"
 	"github.com/gorilla/mux"
@@ -141,6 +140,13 @@ func (i *InternalBuilder) registerOperation(operation *wgpb.Operation) error {
 	preparedPlan := shared.Planner.Plan(shared.Doc, i.definition, operation.Name, shared.Report)
 	shared.Postprocess.Process(preparedPlan)
 
+	hooksPipelineCommonConfig := hooks.PipelineConfig{
+		Client:        i.middlewareClient,
+		Authenticator: hooksAuthenticator,
+		Operation:     operation,
+		Logger:        i.log,
+	}
+
 	switch operation.OperationType {
 	case wgpb.OperationType_QUERY,
 		wgpb.OperationType_MUTATION:
@@ -152,6 +158,13 @@ func (i *InternalBuilder) registerOperation(operation *wgpb.Operation) error {
 		extractedVariables := make([]byte, len(shared.Doc.Input.Variables))
 		copy(extractedVariables, shared.Doc.Input.Variables)
 
+		hooksPipelineConfig := hooks.SynchronousOperationPipelineConfig{
+			PipelineConfig: hooksPipelineCommonConfig,
+			Resolver:       i.resolver,
+			Plan:           p,
+		}
+		hooksPipeline := hooks.NewSynchonousOperationPipeline(hooksPipelineConfig)
+
 		handler := &InternalApiHandler{
 			preparedPlan:       p,
 			operation:          operation,
@@ -159,6 +172,7 @@ func (i *InternalBuilder) registerOperation(operation *wgpb.Operation) error {
 			log:                i.log,
 			resolver:           i.resolver,
 			renameTypeNames:    i.renameTypeNames,
+			hooksPipeline:      hooksPipeline,
 		}
 
 		i.router.Methods(http.MethodPost).Path(apiPath).Handler(handler)
@@ -171,6 +185,13 @@ func (i *InternalBuilder) registerOperation(operation *wgpb.Operation) error {
 		extractedVariables := make([]byte, len(shared.Doc.Input.Variables))
 		copy(extractedVariables, shared.Doc.Input.Variables)
 
+		hooksPipelineConfig := hooks.SubscriptionOperationPipelineConfig{
+			PipelineConfig: hooksPipelineCommonConfig,
+			Resolver:       i.resolver,
+			Plan:           p,
+		}
+		hooksPipeline := hooks.NewSubscriptionOperationPipeline(hooksPipelineConfig)
+
 		handler := &InternalSubscriptionApiHandler{
 			preparedPlan:       p,
 			operation:          operation,
@@ -178,6 +199,7 @@ func (i *InternalBuilder) registerOperation(operation *wgpb.Operation) error {
 			log:                i.log,
 			resolver:           i.resolver,
 			renameTypeNames:    i.renameTypeNames,
+			hooksPipeline:      hooksPipeline,
 		}
 
 		i.router.Methods(http.MethodPost).Path(apiPath).Handler(handler)
@@ -223,8 +245,8 @@ type InternalApiHandler struct {
 	extractedVariables []byte
 	log                *zap.Logger
 	resolver           *resolve.Resolver
-	bearerCache        sync.Map
 	renameTypeNames    []resolve.RenameTypeName
+	hooksPipeline      *hooks.SynchronousOperationPipeline
 }
 
 func (h *InternalApiHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
@@ -286,12 +308,19 @@ func (h *InternalApiHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	buf := pool.GetBytesBuffer()
 	defer pool.PutBytesBuffer(buf)
 
-	resolveErr := h.resolver.ResolveGraphQLResponse(ctx, h.preparedPlan.Response, nil, buf)
-	if done := handleOperationErr(requestLogger, resolveErr, w, "Internal API Handler ResolveGraphQLResponse failed", h.operation); done {
+	resp, err := h.hooksPipeline.Run(ctx, w, r, buf)
+	if done := handleOperationErr(requestLogger, err, w, "hooks pipeline failed", h.operation); done {
 		return
 	}
+
+	if resp.Done {
+		return
+	}
+
 	w.Header().Set("Content-Type", "application/json")
-	_, _ = buf.WriteTo(w)
+	if _, err := w.Write(resp.Data); err != nil {
+		requestLogger.Error("writing response", zap.Error(err))
+	}
 }
 
 type InternalSubscriptionApiHandler struct {
@@ -300,8 +329,8 @@ type InternalSubscriptionApiHandler struct {
 	extractedVariables []byte
 	log                *zap.Logger
 	resolver           *resolve.Resolver
-	bearerCache        sync.Map
 	renameTypeNames    []resolve.RenameTypeName
+	hooksPipeline      *hooks.SubscriptionOperationPipeline
 }
 
 func (h *InternalSubscriptionApiHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
@@ -363,18 +392,13 @@ func (h *InternalSubscriptionApiHandler) ServeHTTP(w http.ResponseWriter, r *htt
 	buf := pool.GetBytesBuffer()
 	defer pool.PutBytesBuffer(buf)
 
-	var (
-		flushWriter *httpFlushWriter
-		ok          bool
-	)
-
-	ctx.Context, flushWriter, ok = getFlushWriter(ctx.Context, ctx.Variables, r, w)
+	flushWriter, ok := getHooksFlushWriter(ctx, r, w, h.hooksPipeline, h.log)
 	if !ok {
 		http.Error(w, "Connection not flushable", http.StatusBadRequest)
 		return
 	}
 
-	err = h.resolver.ResolveGraphQLSubscription(ctx, h.preparedPlan.Response, flushWriter)
+	_, err = h.hooksPipeline.RunSubscription(ctx, flushWriter, r)
 	if err != nil {
 		if errors.Is(err, context.Canceled) {
 			// e.g. client closed connection

testapps/hooks/test/index.test.ts
@@ -1,0 +1,122 @@
+import { afterAll, beforeAll, describe, expect, test } from '@jest/globals';
+import { createTestServer } from '../.wundergraph/generated/testing';
+
+const wg = createTestServer();
+
+beforeAll(() => wg.start());
+afterAll(() => wg.stop());
+
+describe('test recursive operation calls hooks', () => {
+	test('rescursive call', async () => {
+		const client = wg.client();
+		const directResult = await client.query({
+			operationName: 'Continents',
+		});
+		const recursiveResult = await client.query({
+			operationName: 'RecursiveContinents',
+		});
+
+		// RecursiveContinents should end up calling Continents' mockResolve hook
+		const directData = directResult.data!.countries_continents;
+		const recursiveData = recursiveResult.data!.countries_continents;
+		expect(directData.length).toBe(recursiveData.length);
+		expect(directData[0].code).toBe(recursiveData[0].code);
+		expect(directData[0].name).toBe(recursiveData[0].name);
+	});
+});
+
+describe('preResolve chain', () => {
+	test('preResolve failure is reported', async () => {
+		const client = wg.client();
+		const result = await client.query({
+			operationName: 'PreResolveFailure',
+			input: {
+				s: 'initial',
+			},
+		});
+		expect(result.data).toBeUndefined();
+		expect(result.error?.message).toMatch(/preResolve.*failed/i);
+	});
+	test('mutatingPreResolve failure is reported', async () => {
+		const client = wg.client();
+		const result = await client.query({
+			operationName: 'MutatingPreResolveFailure',
+			input: {
+				s: 'initial',
+			},
+		});
+		expect(result.data).toBeUndefined();
+		expect(result.error?.message).toMatch(/mutatingPreResolve.*failed/i);
+	});
+	test('customResolve failure is reported', async () => {
+		const client = wg.client();
+		const result = await client.query({
+			operationName: 'CustomResolveFailure',
+			input: {
+				s: 'initial',
+			},
+		});
+		expect(result.data).toBeUndefined();
+		expect(result.error?.message).toMatch(/customResolve.*failed/i);
+	});
+	test('customResolve', async () => {
+		const client = wg.client();
+		const result = await client.query({
+			operationName: 'CustomResolve',
+			input: {
+				s: 'initial',
+			},
+		});
+		expect(result?.data?.echo_string).toBe('customResolved');
+	});
+	test('customResolve skip', async () => {
+		const skipped = 'skipped';
+		const client = wg.client();
+		const result = await client.query({
+			operationName: 'CustomResolveSkip',
+			input: {
+				s: skipped,
+			},
+		});
+		expect(result?.data?.echo_string).toBe(`string: ${skipped}`);
+	});
+	test('mutatingPreResolve with mockResolve is called', async () => {
+		const client = wg.client();
+		const result = await client.query({
+			operationName: 'PreResolveChain',
+			input: {
+				s: 'initial',
+			},
+		});
+		expect(result.data?.echo_string).toBe('initial.mutatingPreResolve.mockResolve');
+	});
+});
+
+describe('postResolve chain', () => {
+	test('mutatingPostResolve', async () => {
+		const client = wg.client();
+		const result = await client.query({
+			operationName: 'MutatingPostResolve',
+			input: {
+				s: 'initial',
+			},
+		});
+		expect(result.error).toBeUndefined();
+		expect(result.data?.echo_string).toBe('initial.mockResolve.mutatingPostResolve');
+	});
+});
+
+describe('prevent infinite recursion', () => {
+	test('Infinite recursion fails', async () => {
+		const client = wg.client();
+		const result = await client.query({
+			operationName: 'Infinite',
+			input: {
+				s: 'initial',
+			},
+		});
+
+		expect(result.error).toBeDefined();
+		expect(result.data).toBeUndefined();
+	});
+});

packages/sdk/src/client/client.ts
@@ -149,14 +149,14 @@ export class Client {
 
 			switch (response.status) {
 				case 401:
-					return new AuthorizationError(json.errors[0]?.message, response.status);
+					return new AuthorizationError(json.errors[0]?.message.trim(), response.status);
 				case 400:
 					return new InputValidationError(json, response.status);
 				default:
-					return new ResponseError(json.errors[0]?.message || json.message, response.status);
+					return new ResponseError((json.errors[0]?.message || json.message).trim(), response.status);
 			}
 		} catch {
-			return new ResponseError(text.length ? text : 'Response is not OK', response.status);
+			return new ResponseError(text.length ? text.trim() : 'Response is not OK', response.status);
 		}
 	}
 

packages/sdk/src/codegen/templates/typescript/hooks.template.ts
@@ -131,7 +131,7 @@ export interface Queries {
         preResolve?: (hook: {{#if hasInternalInput}}HookRequestWithInput<Injected{{operationName}}Input>{{else}}HookRequest{{/if}}) => Promise<void>;
         {{#if hasInternalInput}} mutatingPreResolve?: (hook: {{#if hasInternalInput}}HookRequestWithInput<Injected{{operationName}}Input>{{else}}HookRequest{{/if}}) => Promise<Injected{{operationName}}Input>;{{/if}}
         postResolve?: (hook: {{#if hasInternalInput}}HookRequestWithInput<Injected{{operationName}}Input>{{else}}HookRequest{{/if}} & HookRequestWithResponse<{{operationName}}Response>) => Promise<void>;
-        customResolve?: (hook: {{#if hasInternalInput}}HookRequestWithInput<Injected{{operationName}}Input>{{else}}HookRequest{{/if}}) => Promise<void | {{operationName}}Response>;
+        customResolve?: (hook: {{#if hasInternalInput}}HookRequestWithInput<Injected{{operationName}}Input>{{else}}HookRequest{{/if}}) => Promise<void | {{operationName}}Response | null>;
         mutatingPostResolve?: (hook: {{#if hasInternalInput}}HookRequestWithInput<Injected{{operationName}}Input>{{else}}HookRequest{{/if}} & HookRequestWithResponse<{{operationName}}Response>) => Promise<{{operationName}}Response>;
         }
     {{/each}}

packages/sdk/src/server/plugins/hooks.ts
@@ -13,6 +13,9 @@ import {
 import { OperationType, WunderGraphConfiguration } from '@wundergraph/protobuf';
 import { RawRequestDefaultExpression, RawServerDefault } from 'fastify/types/utils';
 import { Headers } from '@web-std/fetch';
+import { FastifyRequest } from 'fastify';
+
+const maximumRecursionLimit = 16;
 
 export interface BodyResponse {
 	data?: any;
@@ -125,7 +128,7 @@ const FastifyHooksPlugin: FastifyPluginAsync<FastifyHooksOptions> = async (fasti
 		reply.type('application/json').code(200);
 		try {
 			const maybeHookOut = await config.global?.httpTransport?.onOriginRequest?.hook({
-				...request.ctx,
+				...requestContext(request),
 				operation: {
 					name: request.body.operationName,
 					type: request.body.operationType,
@@ -165,7 +168,7 @@ const FastifyHooksPlugin: FastifyPluginAsync<FastifyHooksOptions> = async (fasti
 		reply.type('application/json').code(200);
 		try {
 			const maybeHookOut = await config.global?.httpTransport?.onOriginResponse?.hook({
-				...request.ctx,
+				...requestContext(request),
 				response: {
 					...request.body.response,
 					headers: new Headers(request.body.response.headers),
@@ -207,7 +210,7 @@ const FastifyHooksPlugin: FastifyPluginAsync<FastifyHooksOptions> = async (fasti
 			try {
 				const resp = await config.global?.wsTransport?.onConnectionInit?.hook({
 					dataSourceId: request.body.dataSourceId,
-					...request.ctx,
+					...requestContext(request),
 					request: {
 						...request.body.request,
 						headers: new Headers(request.body.request.headers),
@@ -235,6 +238,19 @@ const FastifyHooksPlugin: FastifyPluginAsync<FastifyHooksOptions> = async (fasti
 		config.config.api?.operations.filter((op) => op.operationType == OperationType.SUBSCRIPTION).map((op) => op.name) ||
 		[];
 
+	const requestContext = (req: FastifyRequest) => {
+		const body = req.body as any;
+		if (body?.cycleCounter) {
+			if (body.cycleCounter > maximumRecursionLimit) {
+				const errorMessage = `maximum recursion limit reached (${maximumRecursionLimit})`;
+				req.log.error(errorMessage);
+				throw new Error(errorMessage);
+			}
+			req.ctx.internalClient = req.ctx.internalClient.withHeaders({ 'Wg-Cycle-Counter': body.cycleCounter });
+		}
+		return req.ctx;
+	};
+
 	const mockResolve =
 		(
 			operationName: string,
@@ -249,7 +265,7 @@ const FastifyHooksPlugin: FastifyPluginAsync<FastifyHooksOptions> = async (fasti
 			reply.type('application/json').code(200);
 			try {
 				const mutated = await hookFunction({
-					...request.ctx,
+					...requestContext(request),
 					input: request.body.input,
 				});
 				return {
@@ -279,7 +295,7 @@ const FastifyHooksPlugin: FastifyPluginAsync<FastifyHooksOptions> = async (fasti
 			reply.type('application/json').code(200);
 			try {
 				await hookFunction({
-					...request.ctx,
+					...requestContext(request),
 					input: request.body.input,
 				});
 				return {
@@ -308,7 +324,7 @@ const FastifyHooksPlugin: FastifyPluginAsync<FastifyHooksOptions> = async (fasti
 			reply.type('application/json').code(200);
 			try {
 				await hookFunction({
-					...request.ctx,
+					...requestContext(request),
 					input: request.body.input,
 					response: request.body.response,
 				});
@@ -338,7 +354,7 @@ const FastifyHooksPlugin: FastifyPluginAsync<FastifyHooksOptions> = async (fasti
 			reply.type('application/json').code(200);
 			try {
 				const mutatedInput = await hookFunction({
-					...request.ctx,
+					...requestContext(request),
 					input: request.body.input,
 				});
 				return {
@@ -368,7 +384,7 @@ const FastifyHooksPlugin: FastifyPluginAsync<FastifyHooksOptions> = async (fasti
 			reply.type('application/json').code(200);
 			try {
 				const mutatedResponse = await hookFunction({
-					...request.ctx,
+					...requestContext(request),
 					input: request.body.input,
 					response: request.body.response,
 				});
@@ -399,7 +415,7 @@ const FastifyHooksPlugin: FastifyPluginAsync<FastifyHooksOptions> = async (fasti
 			reply.type('application/json').code(200);
 			try {
 				const out = await hookFunction({
-					...request.ctx,
+					...requestContext(request),
 					input: request.body.input,
 				});
 				return {
@@ -483,7 +499,7 @@ const FastifyHooksPlugin: FastifyPluginAsync<FastifyHooksOptions> = async (fasti
 			reply.type('application/json').code(200);
 			try {
 				const result = await handler({
-					...request.ctx,
+					...requestContext(request),
 					file: request.body.file,
 					meta: request.body.meta,
 				});
@@ -507,7 +523,7 @@ const FastifyHooksPlugin: FastifyPluginAsync<FastifyHooksOptions> = async (fasti
 			reply.type('application/json').code(200);
 			try {
 				const result = await handler({
-					...request.ctx,
+					...requestContext(request),
 					file: request.body.file,
 					meta: request.body.meta,
 					error: request.body.error,

pkg/apihandler/apihandler_test.go
@@ -17,6 +17,7 @@ import (
 
 	"github.com/wundergraph/wundergraph/pkg/apicache"
 	"github.com/wundergraph/wundergraph/pkg/authentication"
+	"github.com/wundergraph/wundergraph/pkg/hooks"
 	"github.com/wundergraph/wundergraph/pkg/inputvariables"
 	"github.com/wundergraph/wundergraph/pkg/interpolate"
 	"github.com/wundergraph/wundergraph/pkg/pool"
@@ -39,6 +40,23 @@ func (f *FakeResolver) ResolveGraphQLResponse(ctx *resolve.Context, response *re
 	return
 }
 
+func newPipeline(resolver *FakeResolver, operation *wgpb.Operation) *hooks.SynchronousOperationPipeline {
+	preparedPlan := &plan.SynchronousResponsePlan{
+		Response: &resolve.GraphQLResponse{},
+	}
+	config := hooks.SynchronousOperationPipelineConfig{
+		PipelineConfig: hooks.PipelineConfig{
+			Authenticator: hooksAuthenticator,
+			Operation:     operation,
+			Transformer:   &postresolvetransform.Transformer{},
+			Logger:        zap.NewNop(),
+		},
+		Resolver: resolver,
+		Plan:     preparedPlan,
+	}
+	return hooks.NewSynchonousOperationPipeline(config)
+}
+
 func TestQueryHandler_VariablesIgnore(t *testing.T) {
 
 	interpoalteNothing, err := interpolate.NewStringInterpolator(`{}`)
@@ -51,29 +69,32 @@ func TestQueryHandler_VariablesIgnore(t *testing.T) {
 
 	validateCalled := false
 
-	handler := &QueryHandler{
-		resolver: &FakeResolver{
-			response: []byte(`{"data":{"me":{"name":"Jens"}}}`),
-			validate: func(ctx *resolve.Context, response *resolve.GraphQLResponse, data []byte) {
-				assert.Equal(t, `{"id":123}`, string(ctx.Variables))
-				validateCalled = true
-			},
+	resolver := &FakeResolver{
+		response: []byte(`{"data":{"me":{"name":"Jens"}}}`),
+		validate: func(ctx *resolve.Context, response *resolve.GraphQLResponse, data []byte) {
+			assert.Equal(t, `{"id":123}`, string(ctx.Variables))
+			validateCalled = true
 		},
-		log: zap.NewNop(),
+	}
+	operation := &wgpb.Operation{
+		Name:          "test",
+		OperationType: wgpb.OperationType_QUERY,
+	}
+	handler := &QueryHandler{
+		resolver: resolver,
+		log:      zap.NewNop(),
 		preparedPlan: &plan.SynchronousResponsePlan{
 			Response: &resolve.GraphQLResponse{},
 		},
-		pool: pool.New(),
-		operation: &wgpb.Operation{
-			Name:          "test",
-			OperationType: wgpb.OperationType_QUERY,
-		},
+		pool:                   pool.New(),
+		operation:              operation,
 		rbacEnforcer:           &authentication.RBACEnforcer{},
 		stringInterpolator:     interpoalteNothing,
 		jsonStringInterpolator: interpoalteNothing,
 		variablesValidator:     validateNothing,
 		postResolveTransformer: &postresolvetransform.Transformer{},
 		queryParamsAllowList:   []string{"id"},
+		hooksPipeline:          newPipeline(resolver, operation),
 	}
 
 	srv := httptest.NewServer(handler)
@@ -103,24 +124,28 @@ func TestQueryHandler_ETag(t *testing.T) {
 	validateNothing, err := inputvariables.NewValidator(`{"type":"object","properties":{"id":{"type":"number"}}}`, true)
 	assert.NoError(t, err)
 
+	resolver := &FakeResolver{
+		response: []byte(`{"data":{"me":{"name":"Jens"}}}`),
+	}
+	operation := &wgpb.Operation{
+		Name:          "test",
+		OperationType: wgpb.OperationType_QUERY,
+	}
+
 	handler := &QueryHandler{
-		resolver: &FakeResolver{
-			response: []byte(`{"data":{"me":{"name":"Jens"}}}`),
-		},
-		log: zap.NewNop(),
+		resolver: resolver,
+		log:      zap.NewNop(),
 		preparedPlan: &plan.SynchronousResponsePlan{
 			Response: &resolve.GraphQLResponse{},
 		},
-		pool: pool.New(),
-		operation: &wgpb.Operation{
-			Name:          "test",
-			OperationType: wgpb.OperationType_QUERY,
-		},
+		pool:                   pool.New(),
+		operation:              operation,
 		rbacEnforcer:           &authentication.RBACEnforcer{},
 		stringInterpolator:     interpoalteNothing,
 		jsonStringInterpolator: interpoalteNothing,
 		variablesValidator:     validateNothing,
 		postResolveTransformer: &postresolvetransform.Transformer{},
+		hooksPipeline:          newPipeline(resolver, operation),
 	}
 
 	srv := httptest.NewServer(handler)
@@ -161,6 +186,10 @@ func TestQueryHandler_Caching(t *testing.T) {
 	resolver := &FakeResolver{
 		response: []byte(`{"data":{"me":{"name":"Jens"}}}`),
 	}
+	operation := &wgpb.Operation{
+		Name:          "test",
+		OperationType: wgpb.OperationType_QUERY,
+	}
 
 	handler := &QueryHandler{
 		resolver: resolver,
@@ -175,16 +204,14 @@ func TestQueryHandler_Caching(t *testing.T) {
 			public:               true,
 			staleWhileRevalidate: 0,
 		},
-		cache: cache,
-		operation: &wgpb.Operation{
-			Name:          "test",
-			OperationType: wgpb.OperationType_QUERY,
-		},
+		cache:                  cache,
+		operation:              operation,
 		rbacEnforcer:           &authentication.RBACEnforcer{},
 		stringInterpolator:     interpoalteNothing,
 		jsonStringInterpolator: interpoalteNothing,
 		variablesValidator:     validateNothing,
 		postResolveTransformer: &postresolvetransform.Transformer{},
+		hooksPipeline:          newPipeline(resolver, operation),
 	}
 
 	srv := httptest.NewServer(handler)

pkg/hooks/hooks.go
@@ -11,6 +11,7 @@ import (
 	"log"
 	"net/http"
 	"path"
+	"strconv"
 	"strings"
 	"time"
 
@@ -380,16 +381,20 @@ func (c *Client) doRequest(ctx context.Context, hookResponse HookResponse, actio
 
 	resp, err := c.httpClient.Do(req)
 	if err != nil {
-		return fmt.Errorf("hook %s failed with invalid status code: %d, cause: %w", string(hook), 500, err)
+		return fmt.Errorf("hook %s failed with error: %w", string(hook), err)
+	}
+	defer resp.Body.Close()
+
+	data, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return fmt.Errorf("hook %s failed with reading body with error: %w", string(hook), err)
 	}
 
 	if resp.StatusCode != http.StatusOK {
-		return fmt.Errorf("hook %s failed with invalid status code: %d", string(hook), resp.StatusCode)
+		return fmt.Errorf("hook %s failed with invalid status code: %d (%s)", string(hook), resp.StatusCode, string(data))
 	}
 
-	dec := json.NewDecoder(resp.Body)
-
-	err = dec.Decode(hookResponse)
+	err = json.Unmarshal(data, hookResponse)
 	if err != nil {
 		return fmt.Errorf("hook %s response could not be decoded: %w", string(hook), err)
 	}
@@ -420,4 +425,28 @@ func (c *Client) DoHealthCheckRequest(ctx context.Context) (status bool) {
 	}
 
 	return true
+}
+
+func EncodeData(authenticator Authenticator, r *http.Request, buf []byte, variables []byte, response []byte) []byte {
+	// TODO: This doesn't really reuse the bytes.Buffer storage, refactor it after adding more tests
+	buf = buf[:0]
+	buf = append(buf, []byte(`{"__wg":{}}`)...)
+	if user := authenticator(r.Context()); user != nil {
+		if userJson, err := json.Marshal(user); err == nil {
+			buf, _ = jsonparser.Set(buf, userJson, "__wg", "user")
+		}
+	}
+	if len(variables) > 2 {
+		buf, _ = jsonparser.Set(buf, variables, "input")
+	}
+	if len(response) != 0 {
+		buf, _ = jsonparser.Set(buf, response, "response")
+	}
+	if r != nil {
+		counterHeader := r.Header.Get("Wg-Cycle-Counter")
+		counter, _ := strconv.ParseInt(counterHeader, 10, 64)
+		counterValue := []byte(strconv.FormatInt(counter+1, 10))
+		buf, _ = jsonparser.Set(buf, counterValue, "cycleCounter")
+	}
+	return buf
 }

commit 3258d0172e10b69542a0f875f0edbef81d6903c9

fix: restore postResolveTransform support, add it to internal ops
postResolve transformation were accidentally broken in #669, restore
them. Also, add support for them in internal operations (which we
previously didn't have) and add a couple of tests for them to prevent
any regresions.

Fixes #769

pkg/apihandler/apihandler.go
@@ -507,9 +507,10 @@ func (r *Builder) registerOperation(operation *wgpb.Operation) error {
 	postResolveTransformer := postresolvetransform.NewTransformer(operation.PostResolveTransformations)
 
 	hooksPipelineCommonConfig := hooks.PipelineConfig{
-		Client:    r.middlewareClient,
-		Operation: operation,
-		Logger:    r.log,
+		Client:      r.middlewareClient,
+		Operation:   operation,
+		Transformer: postResolveTransformer,
+		Logger:      r.log,
 	}
 
 	switch operation.OperationType {

pkg/apihandler/internalapihandler.go
@@ -26,6 +26,7 @@ import (
 	"github.com/wundergraph/wundergraph/pkg/interpolate"
 	"github.com/wundergraph/wundergraph/pkg/logging"
 	"github.com/wundergraph/wundergraph/pkg/pool"
+	"github.com/wundergraph/wundergraph/pkg/postresolvetransform"
 	"github.com/wundergraph/wundergraph/pkg/wgpb"
 )
 
@@ -140,10 +141,12 @@ func (i *InternalBuilder) registerOperation(operation *wgpb.Operation) error {
 	preparedPlan := shared.Planner.Plan(shared.Doc, i.definition, operation.Name, shared.Report)
 	shared.Postprocess.Process(preparedPlan)
 
+	postResolveTransformer := postresolvetransform.NewTransformer(operation.PostResolveTransformations)
 	hooksPipelineCommonConfig := hooks.PipelineConfig{
-		Client:    i.middlewareClient,
-		Operation: operation,
-		Logger:    i.log,
+		Client:      i.middlewareClient,
+		Operation:   operation,
+		Transformer: postResolveTransformer,
+		Logger:      i.log,
 	}
 
 	switch operation.OperationType {

testapps/default/test/index.test.ts
@@ -148,3 +148,32 @@ describe('sql', () => {
 		expect(result.data?.row[0].email).toEqual('jannik@wundergraph.com');
 	});
 });
+
+describe('@transform directive', () => {
+	test('@transform in public operation', async () => {
+		const result = await wg.client().query({
+			operationName: 'CountryWeather',
+			input: {
+				countryCode: 'ES',
+			},
+		});
+		expect(result.error).toBeUndefined();
+		const country = result?.data?.country?.[0];
+		expect(country).toBeDefined();
+		expect(country?.capital).toBe('Madrid');
+		expect(country?.weather.temperature?.max).toBeDefined();
+	});
+	test('@transform in internal operation', async () => {
+		const result = await wg.client().query({
+			operationName: 'CountryWeatherViaInternal',
+			input: {
+				countryCode: 'ES',
+			},
+		});
+		expect(result.error).toBeUndefined();
+		const country = result?.data?.data?.country?.[0];
+		expect(country).toBeDefined();
+		expect(country?.capital).toBe('Madrid');
+		expect(country?.weather.temperature?.max).toBeDefined();
+	});
+});