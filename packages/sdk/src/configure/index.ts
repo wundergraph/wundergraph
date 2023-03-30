ac48c8e82d8c12830cfbb6fe602259bd32677ed3
fiam <alberto@garciahierro.com>
2023-03-30 05:49:24 +0100

    fix: make TS operation response schema more forgiving

--- packages/sdk/src/configure/index.ts
+++ packages/sdk/src/configure/index.ts
@@ -1294,6 +1294,7 @@ const typeScriptOperationsResponseSchemas = async (wgDirAbs: string, operations:
 
 	const settings = {
 		required: true,
+		ignoreErrors: true,
 	};
 
 	// XXX: There's no way to silence warnings from TJS, override console.warn
@@ -1303,7 +1304,7 @@ const typeScriptOperationsResponseSchemas = async (wgDirAbs: string, operations:
 	const tsConfigPath = await findUp('tsconfig.json', wgDirAbs);
 	let generator: JsonSchemaGenerator | null = null;
 	if (tsConfigPath) {
-		const tsConfigProgram = programFromConfig(tsConfigPath);
+		const tsConfigProgram = programFromConfig(tsConfigPath, [programPath]);
 		if (tsConfigProgram) {
 			generator = buildGenerator(tsConfigProgram, settings);
 		}
