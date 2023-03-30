c5d3bc8dcd5e1085ba1d7925fc4fe0db32271513
fiam <alberto@garciahierro.com>
2023-03-30 14:56:27 +0100

    test: add a query based test in graphl-hasura-subscriptions

--- examples/graphql-hasura-subscriptions/test/index.test.ts
+++ examples/graphql-hasura-subscriptions/test/index.test.ts
@@ -2,29 +2,67 @@ import { afterAll, beforeAll, describe, expect, test } from '@jest/globals';
 
 import { ClientOperationErrors, ClientResponse } from '@wundergraph/sdk/client';
 
-import { TodoResponseData } from '../.wundergraph/generated/models';
+import { TodosSubscriptionResponseData } from '../.wundergraph/generated/models';
 import { createTestServer } from '../.wundergraph/generated/testing';
 
 const wg = createTestServer({ fetch: fetch as any });
 beforeAll(() => wg.start());
 afterAll(() => wg.stop());
 
+// Only run tests if we have a valid token
+const testIfToken = !!process.env.HASURA_BEARER_TOKEN ? test : test.skip;
+
 describe('test hasura subscriptions', () => {
-	test('todo', async () => {
-		if (!process.env.HASURA_BEARER_TOKEN) {
-			// Skip
-			return;
-		}
+	testIfToken('todos subscribe once', async () => {
 		const result = (await wg.client().subscribe(
 			{
-				operationName: 'Todo',
+				operationName: 'TodosSubscription',
 				subscribeOnce: true,
 			},
 			() => {}
-		)) as ClientResponse<TodoResponseData | undefined, ClientOperationErrors | undefined>;
+		)) as ClientResponse<TodosSubscriptionResponseData | undefined, ClientOperationErrors | undefined>;
 
 		expect(result.error).toBeUndefined();
 		expect(result.data?.hasura_todos.length ?? 0 > 0).toBeTruthy();
-		console.log(result);
+	});
+
+	testIfToken('todos subscribe and cancel', async () => {
+		const abortController = new AbortController();
+		const customFetch = (req: RequestInfo | URL) => fetch(req, { signal: abortController.signal });
+		const server = createTestServer({ fetch: customFetch });
+		await server.start();
+		let receivedData = false;
+		try {
+			await server.client().subscribe(
+				{
+					operationName: 'TodosSubscription',
+				},
+				(resp) => {
+					expect(resp.error).toBeUndefined();
+					expect(resp.data).toBeDefined();
+					expect(resp.data?.hasura_todos.length ?? 0 > 0).toBeTruthy();
+					expect(resp.data?.hasura_todos?.[0].title).toBeTruthy();
+					receivedData = true;
+					abortController.abort();
+				}
+			);
+		} catch {
+		} finally {
+			await server.stop();
+		}
+
+		expect(receivedData).toBeTruthy();
+	});
+
+	testIfToken('todos query', async () => {
+		const result = await wg.client().query({
+			operationName: 'TodosQuery',
+			input: {
+				limit: 10,
+			},
+		});
+
+		expect(result.error).toBeUndefined();
+		expect(result.data?.hasura_todos.length).toBe(10);
 	});
 });
