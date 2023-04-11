261968d436a7036629efd8073448a7e184930cb3
DaniAkash <s.daniakash@gmail.com>
2023-04-11 14:39:28 +0530

    fix: formatting errors

--- examples/vite-svelte/vite.config.ts
+++ examples/vite-svelte/vite.config.ts
@@ -1,13 +1,13 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { defineConfig } from 'vite';
+import { svelte } from '@sveltejs/vite-plugin-svelte';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
-  optimizeDeps: {
-    include: ['@wundergraph/svelte-query', '@wundergraph/sdk/client'],
-    esbuildOptions: {
-      target: 'es2020',
-    },
-  },
-})
+	plugins: [svelte()],
+	optimizeDeps: {
+		include: ['@wundergraph/svelte-query', '@wundergraph/sdk/client'],
+		esbuildOptions: {
+			target: 'es2020',
+		},
+	},
+});
