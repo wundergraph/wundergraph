8d295f9c8f44738d1c28f4f54657a125bd94b899
Dani Akash <DaniAkash@users.noreply.github.com>
2023-04-11 15:00:46 +0530

    fix: formatting errors introduced in svelte examples (#817)
    fix: formatting errors

--- examples/sveltekit/src/routes/+layout.ts
+++ examples/sveltekit/src/routes/+layout.ts
@@ -1,15 +1,15 @@
-import { browser } from '$app/environment'
-import { QueryClient } from '@tanstack/svelte-query'
-import type { LayoutLoad } from './$types'
+import { browser } from '$app/environment';
+import { QueryClient } from '@tanstack/svelte-query';
+import type { LayoutLoad } from './$types';
 
 export const load: LayoutLoad = async () => {
-    const queryClient = new QueryClient({
-        defaultOptions: {
-            queries: {
-                enabled: browser,
-            },
-        },
-    })
+	const queryClient = new QueryClient({
+		defaultOptions: {
+			queries: {
+				enabled: browser,
+			},
+		},
+	});
 
-    return { queryClient }
-}
+	return { queryClient };
+};
