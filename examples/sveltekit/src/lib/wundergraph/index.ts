c1563b888d2f265d9c9c59fa08ee38078b6a169c
DaniAkash <s.daniakash@gmail.com>
2023-04-11 13:04:58 +0530

    fix: temporarily exclude type check error in factory config file

--- examples/sveltekit/src/lib/wundergraph/index.ts
+++ examples/sveltekit/src/lib/wundergraph/index.ts
@@ -4,7 +4,7 @@ import type { Operations } from '../.wundergraph/generated/client';
 
 const client = createClient();
 
-const { createFileUpload, createMutation, createQuery, createSubscription, getAuth, getUser, queryKey } =
+const { createFileUpload, createMutation, createQuery, createSubscription, getAuth, getUser, queryKey, prefetchQuery } =
 	createSvelteClient<Operations>(client);
 
-export { createFileUpload, createMutation, createQuery, createSubscription, getAuth, getUser, queryKey };
+export { createFileUpload, createMutation, createQuery, createSubscription, getAuth, getUser, queryKey, prefetchQuery };
