33ef70596322064c0ff74d60231e1ecb3609cb08
Leonardo Alves <hello@leoalves.net>
2023-04-11 21:15:58 -0400

    refactor: changed to prefetch query from @wundergraph/svelte-query

--- examples/sveltekit-oidc-cookies-auth/src/routes/+page.ts
+++ examples/sveltekit-oidc-cookies-auth/src/routes/+page.ts
@@ -1,21 +1,14 @@
-import { createClient } from '$lib/generated/client'
 import { createWGClient } from '$lib/svelte-query'
 import type { PageLoad } from './$types'
 
 export const load: PageLoad = async ({ parent, fetch }) => {
-  const { queryClient } = await parent()
+  const { queryClient } = await parent()  
+  const { prefetchQuery } = createWGClient(fetch)
   
-  const {queryKey} = createWGClient(fetch)
-  const client = createClient({
-    customFetch: fetch
-  })  
-
-  await queryClient.prefetchQuery({
-    queryKey: queryKey({
-        operationName: 'user/Me'
-    }),
-    queryFn: () => client.query({
-        operationName: 'user/Me'
-    }) as any
-  })
+  await prefetchQuery(
+    {
+      operationName: 'user/Me'
+    },
+    queryClient
+  );
 }
