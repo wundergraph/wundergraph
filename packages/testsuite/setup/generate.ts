c911f74d8e332abfbe937e379a1ec44b84799359
StarpTech <deusdustin@gmail.com>
2023-04-16 13:39:55 +0200

    chore: improve

--- packages/testsuite/setup/generate.ts
+++ packages/testsuite/setup/generate.ts
@@ -1,11 +1,22 @@
-// @ts-ignore
 import execa from 'execa';
 import { join } from 'node:path';
 import { wunderctlBinaryPath } from '@wundergraph/wunderctl';
 
 export async function Setup(path: string) {
+	await Clean(path);
 	await Generate(path);
 	await TscCheck(path);
+}
+
+export async function Clean(path: string) {
+	const cmdArgs = ['clean'];
+
+	await execa(wunderctlBinaryPath(), cmdArgs, {
+		encoding: 'utf-8',
+		extendEnv: true,
+		cwd: join(process.cwd(), path),
+		stdio: 'inherit',
+	});
 }
 
 export async function Generate(path: string) {
