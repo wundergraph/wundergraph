ba1a3a262b1f1336596cc854a46b744c3343d77d
fiam <alberto@garciahierro.com>
2023-04-20 15:32:57 +0100

    chore: test again

--- packages/protobuf/wgpb/wundernode_config.ts
+++ packages/protobuf/wgpb/wundernode_config.ts
@@ -1030,7 +1030,6 @@ export interface FetchConfiguration {
 }
 
 export interface FetchConfiguration_HeaderEntry {
-  /* Just making a random change DO NOT MERGE */
   key: string;
   value: HTTPHeader | undefined;
 }
