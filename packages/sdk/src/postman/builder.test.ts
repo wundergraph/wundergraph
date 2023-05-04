3a41f05781df0cc31da1aea62c60acfb4b4a4acb
fiam <alberto@garciahierro.com>
2023-05-04 19:29:35 +0100

    fix: don't skip second level operations in Postman collections, skip internal
    Rewrite the algorithm to use a simpler method to build the directories and the
    child/parent relationships. Skip internal operations while generating the
    collection.

--- packages/sdk/src/postman/builder.test.ts
+++ packages/sdk/src/postman/builder.test.ts
@@ -1,5 +1,5 @@
+import fs from 'fs';
 import { OperationExecutionEngine, OperationType } from '@wundergraph/protobuf';
-import { Collection } from 'postman-collection';
 import { PostmanBuilder } from './builder';
 
 const operations = [
@@ -64,8 +64,8 @@ const operations = [
 		},
 	},
 	{
-		Name: 'GetUser',
-		PathName: 'location/query/GetUser',
+		Name: 'GetUser2',
+		PathName: 'location/query/GetUser2',
 		OperationType: OperationType.QUERY,
 		ExecutionEngine: OperationExecutionEngine.ENGINE_GRAPHQL,
 		VariablesSchema: {
@@ -77,7 +77,7 @@ const operations = [
 	},
 	{
 		Name: 'GetCart',
-		PathName: 'location/query/GetUser',
+		PathName: 'location/query/GetCart',
 		OperationType: OperationType.QUERY,
 		ExecutionEngine: OperationExecutionEngine.ENGINE_GRAPHQL,
 		VariablesSchema: {
@@ -111,10 +111,49 @@ const operations = [
 			definitions: {},
 		},
 	},
+	{
+		Name: 'Internal',
+		PathName: 'internal/query/internal',
+		Internal: true,
+		OperationType: OperationType.QUERY,
+		ExecutionEngine: OperationExecutionEngine.ENGINE_NODEJS,
+		VariablesSchema: {
+			type: 'object',
+			properties: {},
+			additionalProperties: false,
+			definitions: {},
+		},
+	},
 ] as any;
 
-test('PostmanBuilder', async () => {
+const countPostmanOperations = (obj: any) => {
+	let count = 0;
+	if (obj.request) {
+		count += 1;
+	}
+	const item = obj.item;
+	if (item) {
+		if (Array.isArray(item)) {
+			for (const element of item) {
+				count += countPostmanOperations(element);
+			}
+		} else {
+			count += countPostmanOperations(item);
+		}
+	}
+	return count;
+};
+
+test.only('PostmanBuilder', async () => {
 	const result = PostmanBuilder(operations, { baseURL: 'http://localhost:9991' });
+
+	const internalOperationsCount = 1;
+	const c = countPostmanOperations(result.toJSON());
+	expect(c).toBe(operations.length - internalOperationsCount);
+
+	if (process.env.WRITE_POSTMAN_COLLECTION) {
+		fs.writeFileSync('postman.json', JSON.stringify(result.toJSON(), null, 2), 'utf-8');
+	}
 
 	expect(result).toMatchSnapshot();
 });
