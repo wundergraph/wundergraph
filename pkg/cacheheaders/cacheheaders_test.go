9951845424ae076c63c257d6e58bdd447c7fd0f6
fiam <alberto@garciahierro.com>
2023-05-08 21:04:44 +0100

    fix: add missing files

---
+++ pkg/cacheheaders/cacheheaders_test.go
@@ -0,0 +1,56 @@
+package cacheheaders
+
+import (
+	"net/http/httptest"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCacheControl(t *testing.T) {
+	testCases := []struct {
+		cacheControl *CacheControl
+		header       string
+	}{
+		{&CacheControl{
+			Public:               true,
+			MaxAge:               42,
+			StaleWhileRevalidate: 43,
+		},
+			"public, max-age=42, stale-while-revalidate=43",
+		},
+		{&CacheControl{},
+			"private, max-age=0, stale-while-revalidate=0",
+		},
+		{
+			nil,
+			"",
+		},
+	}
+	for _, tc := range testCases {
+		t.Run(tc.header, func(t *testing.T) {
+			var rc httptest.ResponseRecorder
+			headers := New(tc.cacheControl, "")
+			headers.Set(&rc, nil)
+			result := rc.Result()
+			assert.Equal(t, tc.header, result.Header.Get("Cache-Control"))
+		})
+	}
+}
+
+func TestETag(t *testing.T) {
+	headers := New(nil, "")
+
+	var rc1 httptest.ResponseRecorder
+	headers.Set(&rc1, nil)
+	result1 := rc1.Result()
+	assert.Len(t, result1.Header["ETag"], 0)
+
+	var rc2 httptest.ResponseRecorder
+	headers.Set(&rc2, []byte("something"))
+	result2 := rc2.Result()
+	assert.Len(t, result2.Header["ETag"], 1)
+
+	// Our ETag headers should be weak etags
+	assert.Equal(t, "W", string(result2.Header["ETag"][0][0]))
+}
