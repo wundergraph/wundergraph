4dc415ae897f9788c32fda41b2a9b266454b583d
Pagebakers <eelco@appulse.nl>
2023-05-09 13:17:43 +0200

    chore: revert

--- testapps/openapi/lib/react-query.ts
+++
@@ -1,6 +0,0 @@
-import { createHooks } from '@wundergraph/react-query';
-import { createClient, Operations } from '../components/generated/client';
-const client = createClient(); // Typesafe WunderGraph client
-
-export const { useQuery, useMutation, useSubscription, useUser, useFileUpload, useAuth, queryKey } =
-	createHooks<Operations>(client);
