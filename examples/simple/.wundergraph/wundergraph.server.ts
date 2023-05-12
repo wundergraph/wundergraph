16de824f6e7ee0dcd64f74d668733e55cce15be0
fiam <alberto@garciahierro.com>
2023-05-12 15:58:16 +0100

    fix: accidental changes in simple example

--- examples/simple/.wundergraph/wundergraph.server.ts
+++ examples/simple/.wundergraph/wundergraph.server.ts
@@ -1,24 +1,12 @@
-import { GraphQLObjectType, GraphQLSchema, GraphQLString } from 'graphql';
 import { configureWunderGraphServer } from '@wundergraph/sdk/server';
-
-export class MyContext {
-	greet() {
-		console.log('say hello');
-	}
-}
-
-export const makeContext = async () => {
-	return new MyContext();
-};
 
 export default configureWunderGraphServer(() => ({
 	hooks: {
 		queries: {
 			Countries: {
-				preResolve: async ({ operations, context }) => {},
+				preResolve: async ({ operations }) => {},
 			},
 		},
 		mutations: {},
 	},
-	context: makeContext,
 }));
