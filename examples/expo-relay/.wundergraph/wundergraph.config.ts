1e5bc43feebed2bcdea47724236dad1e23f7bec7
DaniAkash <s.daniakash@gmail.com>
2023-05-15 11:12:02 +0530

    feat: added wundergraph to relay example

---
+++ examples/expo-relay/.wundergraph/wundergraph.config.ts
@@ -0,0 +1,31 @@
+import { configureWunderGraphApplication, cors, EnvironmentVariable, introspect, templates } from '@wundergraph/sdk';
+import server from './wundergraph.server';
+import operations from './wundergraph.operations';
+
+const countries = introspect.graphql({
+	apiNamespace: 'countries',
+	url: 'https://countries.trevorblades.com/',
+});
+
+// configureWunderGraph emits the configuration
+configureWunderGraphApplication({
+	apis: [countries],
+	server,
+	operations,
+	generate: {
+		codeGenerators: [],
+	},
+	cors: {
+		...cors.allowAll,
+		allowedOrigins:
+			process.env.NODE_ENV === 'production'
+				? [
+						// change this before deploying to production to the actual domain where you're deploying your app
+						'http://localhost:3000',
+				  ]
+				: ['http://localhost:3000', new EnvironmentVariable('WG_ALLOWED_ORIGIN')],
+	},
+	security: {
+		enableGraphQLEndpoint: process.env.NODE_ENV !== 'production' || process.env.GITPOD_WORKSPACE_ID !== undefined,
+	},
+});
