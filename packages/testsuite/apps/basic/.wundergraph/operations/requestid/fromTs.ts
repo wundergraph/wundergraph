25b5ed9b70ac3ca1246c061803210b5b2546b5c7
fiam <alberto@garciahierro.com>
2023-05-24 09:36:13 +0100

    fix: remove debug print

--- packages/testsuite/apps/basic/.wundergraph/operations/requestid/fromTs.ts
+++ packages/testsuite/apps/basic/.wundergraph/operations/requestid/fromTs.ts
@@ -3,7 +3,6 @@ import { createOperation } from '../../generated/wundergraph.factory';
 export default createOperation.query({
 	handler: async ({ operations }) => {
 		const { data, error } = await operations.query({ operationName: 'requestid/ts' });
-		console.log('DE', data, error);
 		if (error) {
 			throw error;
 		}
