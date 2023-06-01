047bf68c4c28293746395383faba1fb90a7c575d
Eelco Wiersma <contact@pagebakers.nl>
2023-06-01 20:49:15 +0200

    chore: indentation

--- packages/testsuite/apps/orm/.wundergraph/operations/clientRequestContentLength.ts
+++ packages/testsuite/apps/orm/.wundergraph/operations/clientRequestContentLength.ts
@@ -2,7 +2,10 @@ import { createOperation } from '../generated/wundergraph.factory';
 
 export default createOperation.query({
 	handler: async ({ graph }) => {
-		const length = await graph.from('echo').query('clientRequestContentLength').exec();
+		const length = await graph
+			.from('echo')
+			.query('clientRequestContentLength')
+			.exec();
 		return {
 			length,
 		};
