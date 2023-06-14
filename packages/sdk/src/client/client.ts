a57095be4d865c57295365b9b092c6f6a8d1f764
fiam <alberto@garciahierro.com>
2023-06-14 13:16:49 +0100

    refactor: use a custom error type for signaling there's no user available in fetchUser()

--- packages/sdk/src/client/client.ts
+++ packages/sdk/src/client/client.ts
@@ -22,6 +22,7 @@ import {
 	AuthorizationError,
 	ValidationResponseJSON,
 	ClientOperationErrorCodes,
+	NoUserError,
 } from './errors';
 
 // We follow https://docs.wundergraph.com/docs/architecture/wundergraph-rpc-protocol-explained
@@ -371,7 +372,7 @@ export class Client {
 
 		if (response.status == 204) {
 			// No user is signed in. Keep this in sync with pkg/authentication/authentication.go
-			throw new AuthorizationError();
+			throw new NoUserError({ statusCode: response.status });
 		}
 
 		return response.json();
