afeeb3596874cd20bc00628133a2fd5c7e739016
fiam <alberto@garciahierro.com>
2023-06-17 11:54:13 +0100

    chore: re-enable sveltekit-oidc-cookies-auth

--- scripts/test-examples/all.sh
+++ scripts/test-examples/all.sh
@@ -20,7 +20,7 @@ set -e
 SKIP="faunadb-nextjs graphql-hasura-subscriptions inject-bearer"
 
 # These are broken
-SKIP="${SKIP} nextjs-todos remix nuxt sveltekit-oidc-cookies-auth"
+SKIP="${SKIP} nextjs-todos remix nuxt"
 
 ## Doesn't support monorepo
 SKIP="${SKIP} expo-swr"
