6fa19eb1afbbddc5cf1659a4c1ea9bb9d4aa1ca9
fiam <alberto@garciahierro.com>
2023-07-11 18:38:52 +0100

    fix: import name in template

--- packages/sdk/src/dynamic-router/codegen.template.ts
+++ packages/sdk/src/dynamic-router/codegen.template.ts
@@ -1,6 +1,6 @@
 //language=handlebars
 export const handlebarTemplate = `
-import { DATA_SOURCES } from './hooks'
+import { DATA_SOURCES } from './wundergraph.hooks'
 
 declare module '@wundergraph/sdk/dynamic-router' {
 	export interface RouteMatcher {
