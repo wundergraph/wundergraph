a6687a827e77ac8c9fcdcb21e12b65895367a347
Jens Neuse <jens.neuse@gmx.de>
2023-07-12 17:05:25 +0200

    chore: add description to generated openapi spec

--- packages/sdk/src/openapibuilder/index.ts
+++ packages/sdk/src/openapibuilder/index.ts
@@ -350,11 +350,13 @@ export class OpenApiBuilder {
 				case OperationType.QUERY:
 				case OperationType.SUBSCRIPTION:
 					opPath = {
+						description: op.Description ? op.Description : undefined,
 						get: this.queryOperation(op),
 					};
 					break;
 				case OperationType.MUTATION:
 					opPath = {
+						description: op.Description ? op.Description : undefined,
 						post: this.mutationOperation(op),
 					};
 					break;
