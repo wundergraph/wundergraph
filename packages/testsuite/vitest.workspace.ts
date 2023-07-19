c173fa6eb8eb1d5cb3e5055f831b762783d79b63
fiam <alberto@garciahierro.com>
2023-07-19 22:17:16 +0100

    fix: remove duplicated entries from vitest workspace

--- packages/testsuite/vitest.workspace.ts
+++ packages/testsuite/vitest.workspace.ts
@@ -26,14 +26,6 @@ export default defineWorkspace([
 		},
 	},
 	{
-		root: 'apps/basic',
-		test: {
-			globalSetup: 'apps/basic/test-setup.ts',
-			testTimeout: 15000,
-			hookTimeout: 20000,
-		},
-	},
-	{
 		root: 'apps/hooks',
 		test: {
 			globalSetup: 'apps/hooks/test-setup.ts',
@@ -94,55 +86,6 @@ export default defineWorkspace([
 		root: 'apps/schema-extensions',
 		test: {
 			globalSetup: 'apps/schema-extensions/test-setup.ts',
-			testTimeout: 15000,
-			hookTimeout: 20000,
-		},
-	},
-	{
-		root: 'apps/hooks',
-		test: {
-			globalSetup: 'apps/hooks/test-setup.ts',
-			testTimeout: 15000,
-			hookTimeout: 20000,
-		},
-	},
-	{
-		root: 'apps/http-proxy',
-		test: {
-			globalSetup: 'apps/http-proxy/test-setup.ts',
-			// High timeout because we're testing code generation
-			testTimeout: 60000,
-			hookTimeout: 20000,
-		},
-	},
-	{
-		root: 'apps/mock',
-		test: {
-			globalSetup: 'apps/mock/test-setup.ts',
-			testTimeout: 15000,
-			hookTimeout: 20000,
-		},
-	},
-	{
-		root: 'apps/openapi',
-		test: {
-			globalSetup: 'apps/openapi/test-setup.ts',
-			testTimeout: 15000,
-			hookTimeout: 20000,
-		},
-	},
-	{
-		root: 'apps/opentelemetry',
-		test: {
-			globalSetup: 'apps/opentelemetry/test-setup.ts',
-			testTimeout: 15000,
-			hookTimeout: 20000,
-		},
-	},
-	{
-		root: 'apps/orm',
-		test: {
-			globalSetup: 'apps/orm/test-setup.ts',
 			testTimeout: 15000,
 			hookTimeout: 20000,
 		},
