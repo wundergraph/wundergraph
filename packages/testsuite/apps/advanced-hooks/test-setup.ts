6a45c438183baa495e4e0e43d12f696195fdc9dd
fiam <alberto@garciahierro.com>
2023-07-20 16:27:25 +0100

    fix: app path in vitest test setup

--- packages/testsuite/apps/advanced-hooks/test-setup.ts
+++ packages/testsuite/apps/advanced-hooks/test-setup.ts
@@ -4,6 +4,6 @@ import { createGraphQLTestServers } from './servers';
 export default async function () {
 	const servers = createGraphQLTestServers();
 	await Promise.all(servers.map((s) => s.start()));
-	await Setup('apps/dynamic-router');
+	await Setup('apps/advanced-hooks');
 	await Promise.all(servers.map((s) => s.stop()));
 }
