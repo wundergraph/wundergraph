c709880361a14ae5fd6cf3b49d823e107b463a1f
Pagebakers <eelco@appulse.nl>
2023-08-23 15:39:42 +0200

    chore: remove react query

--- testapps/nextjs-integrations/pages/_app.tsx
+++ testapps/nextjs-integrations/pages/_app.tsx
@@ -1,12 +1,7 @@
 import '../styles/globals.css';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-const queryClient = new QueryClient();
+
 function MyApp({ Component, pageProps }) {
-	return (
-		<QueryClientProvider client={queryClient}>
-			<Component {...pageProps} />
-		</QueryClientProvider>
-	);
+	return <Component {...pageProps} />;
 }
 
 export default MyApp;
