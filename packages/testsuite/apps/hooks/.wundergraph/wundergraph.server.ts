2c51ba3c45eee0534106c3b74feffa5c60ab1da5
Pagebakers <eelco@appulse.nl>
2023-08-30 17:31:42 +0200

    chore: fix test

--- packages/testsuite/apps/hooks/.wundergraph/wundergraph.server.ts
+++ packages/testsuite/apps/hooks/.wundergraph/wundergraph.server.ts
@@ -11,6 +11,10 @@ export default configureWunderGraphServer(() => ({
 						operationName: 'Infinite',
 						input: hook.input,
 					});
+
+					if (result.error) {
+						throw result.error;
+					}
 				},
 			},
 			PreResolveFailure: {
