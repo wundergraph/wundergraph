617dee85048fbb5d3ed037f5cd517c7b5f109212
fiam <alberto@garciahierro.com>
2023-08-30 16:59:42 +0100

    fix: wait for the poll interval when a function returns the same output to a live query
    If the output was the same, we were skipping the sleep between poll attempts. To
    prevent this in the future, use a timer based approach instead.

---
+++ packages/testsuite/apps/basic/livequery.test.ts
@@ -0,0 +1,33 @@
+import { beforeAll, describe, expect, it } from 'vitest';
+import { createTestServer } from './.wundergraph/generated/testing';
+
+const wg = createTestServer({
+	dir: __dirname,
+});
+
+beforeAll(async () => {
+	await wg.start();
+
+	return async () => {
+		await wg.stop();
+	};
+});
+
+describe('Live queries', () => {
+	it('should respect interval', async () => {
+		// Reset the call counter
+		await wg.client().query({ operationName: 'functions/trigger-livequery-bug', input: { reset: true } });
+		// Now start a live query, which shouldn't send more the one call per second
+		const abortController = new AbortController();
+		fetch(wg.url('/operations/functions/trigger-livequery-bug?wg_live'), { signal: abortController.signal });
+		// Sleep for 2 seconds
+		await new Promise((resolve) => setTimeout(resolve, 1100));
+		abortController.abort();
+		// Now check the call counter
+		const result = await wg
+			.client()
+			.query({ operationName: 'functions/trigger-livequery-bug', input: { output: true } });
+		// reset + 1 live query trigger + call to retrieve output = 3
+		expect(parseInt(result.data!, 10)).toBe(3);
+	});
+});
