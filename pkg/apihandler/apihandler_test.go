7dd532b177956798558394ce004e1dd3fa1b74d6
Alberto García Hierro <alberto@garciahierro.com>
2023-09-01 12:01:01 +0100

    fix: don't interfere with terminal after running wunderctl up (#1245)

--- pkg/apihandler/apihandler_test.go
+++ pkg/apihandler/apihandler_test.go
@@ -593,12 +593,13 @@ func TestFunctionsHandler_Live(t *testing.T) {
 	validateNothing, err := inputvariables.NewValidator(inputSchema, true)
 	assert.NoError(t, err)
 
-	hookServerRequestCount := 0
+	var hookServerRequestCount atomic.Int64
 
 	fakeHookServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusOK)
-		_, _ = w.Write([]byte(`{"response":{"data":{"me":{"name":"Jens","bio":"Founder & CEO of WunderGraph","counter":` + strconv.Itoa(hookServerRequestCount) + `}}}}`))
-		hookServerRequestCount++
+		count := int(hookServerRequestCount.Load())
+		_, _ = w.Write([]byte(`{"response":{"data":{"me":{"name":"Jens","bio":"Founder & CEO of WunderGraph","counter":` + strconv.Itoa(count) + `}}}}`))
+		hookServerRequestCount.Add(1)
 	}))
 
 	defer fakeHookServer.Close()
@@ -649,7 +650,7 @@ func TestFunctionsHandler_Live(t *testing.T) {
 	data, err := ioutil.ReadAll(res.Body)
 	assert.Equal(t, context.DeadlineExceeded, err)
 	assert.Equal(t, "{\"data\":{\"me\":{\"name\":\"Jens\",\"bio\":\"Founder & CEO of WunderGraph\",\"counter\":0}}}\n\n{\"data\":{\"me\":{\"name\":\"Jens\",\"bio\":\"Founder & CEO of WunderGraph\",\"counter\":1}}}\n\n{\"data\":{\"me\":{\"name\":\"Jens\",\"bio\":\"Founder & CEO of WunderGraph\",\"counter\":2}}}\n\n", string(data))
-	assert.Equal(t, 3, hookServerRequestCount)
+	assert.Equal(t, 3, int(hookServerRequestCount.Load()))
 }
 
 func TestFunctionsHandler_Live_JSONPatch(t *testing.T) {
