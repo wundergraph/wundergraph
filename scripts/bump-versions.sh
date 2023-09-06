011a08ce5ed979863d0555e9d20db165a3ad8503
fiam <alberto@garciahierro.com>
2023-09-06 08:56:27 +0100

    chore: improve script for bumping SDK in examples
    - Add arguments to automatically create a branch, commit and PR
    - Rename script to bump-sdk-in-examples.sh

--- scripts/bump-versions.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -e
-
-printf "{\"name\": \"examples\"}" > examples/package.json
-printf "packages:\n  - '*'" > examples/pnpm-workspace.yaml
-cd examples && pnpm up -r "@wundergraph/*@latest"
-rm -rf node_modules package.json pnpm-workspace.yaml pnpm-lock.yaml
