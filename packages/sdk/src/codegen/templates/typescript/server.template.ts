d1b95962a81418200969e8e69c2df982c6e305c8
Eelco Wiersma <contact@pagebakers.nl>
2023-09-06 13:40:18 +0200

    feat: improved context factory types (#1250)
    * chore: fix typo
    
    * chore: remove all references to internalClient
    
    * chore: fix test
    
    * fix: test
    
    * chore: update test
    
    * feat: make context factory type definition explicit
    
    * feat: improve generated types
    
    * chore: add comments
    
    * chore: remove test code
    
    ---------
    
    Co-authored-by: Alberto García Hierro <alberto@garciahierro.com>

--- packages/sdk/src/codegen/templates/typescript/server.template.ts
+++ packages/sdk/src/codegen/templates/typescript/server.template.ts
@@ -6,6 +6,7 @@ import type { InternalOperationsClient } from "./wundergraph.internal.operations
 import type { CustomClaims } from "./claims";
 import type {
 	BaseRequestContext,
+	CustomContext,
 	GraphQLServerConfig,
 	WunderGraphUser,
 	WunderGraphServerConfig,
@@ -16,28 +17,28 @@ export type Role = {{{ roleDefinitions }}};
 
 export interface User extends WunderGraphUser<Role, CustomClaims> {}
 
-export interface Config<TCustomContext> {
-    hooks: HooksConfig<TCustomContext>;
-    graphqlServers?: Omit<GraphQLServerConfig, 'routeUrl'>[];
-}
+/**
+ * We extract the custom context defined by the user
+ */
+type RequestContext = CustomContext extends { request: infer R } ? R : any;
+type GlobalContext = CustomContext extends { global: infer G } ? G : any;
 
-export interface OutputConfig<TCustomContext> {
-    hooks: HooksConfig<TCustomContext>;
-    graphqlServers?: (GraphQLServerConfig & { url: string })[];
-}
-
-export interface GraphQLExecutionContext<TCustomContext = any> {
-    wundergraph: BaseRequestContext<User, InternalOperationsClient, TCustomContext>;
+/**
+ * Can be used for custom GraphQL server execution context
+ * @see https://docs.wundergraph.com/docs/wundergraph-server-ts-reference/custom-graphql-servers
+ */
+export interface GraphQLExecutionContext {
+    wundergraph: BaseRequestContext<User, InternalOperationsClient, RequestContext>;
 }
 
 declare module "@wundergraph/sdk/server" {
-	export function configureWunderGraphServer<TRequestContext = any, TGlobalContext = any>(
+	export function configureWunderGraphServer(
 		configWrapper: () => WunderGraphServerConfig<
-			HooksConfig<TRequestContext>,
+			HooksConfig<RequestContext>,
 			WebhooksConfig,
-			TRequestContext,
-			TGlobalContext
+			RequestContext,
+			GlobalContext
 		>
-	): WunderGraphHooksAndServerConfig<any, any, TRequestContext, TGlobalContext>;
+	): WunderGraphHooksAndServerConfig<any, any, RequestContext, GlobalContext>;
 }
 `;
