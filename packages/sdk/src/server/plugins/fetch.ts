a3ca1b0a8ba090513f848b6e9bd60365d9ef9d5b
Alberto García Hierro <alberto@garciahierro.com>
2023-09-14 13:51:09 +0100

    feat: log requests and responses to OpenAPI and SOAP upstreams (#1272)
    Log the method, headers, status codes and contents. These requests are unlikely
    to carry binary data, so logging the payloads should not interfere with the terminal
    output.
    
    Requests and responses are logged at the debug level, while errors while calling
    the underlying fetch are logged at warn error and rethrown.

---
+++ packages/sdk/src/server/plugins/fetch.ts
@@ -0,0 +1,39 @@
+import { FastifyBaseLogger } from 'fastify';
+
+type FetchFn = (input: RequestInfo | URL, init?: RequestInit) => Promise<Response>;
+
+/**
+ * Create a fetch function that logs its requests and responses to the
+ * given logger.
+ *
+ * Requests and responses are logged at debug level. If fetch throws, the
+ * error is logged at warn level and the exception is propagated.
+ *
+ * @param logger Logger to send the logged messages to
+ * @param fetch Fetch function to wrap
+ * @returns A wrapper to fetch that logs requests and responses
+ */
+export const loggedFetch = <F extends FetchFn>(logger: FastifyBaseLogger, fetchFn: F): F => {
+	const fn = async (...args: [RequestInfo | URL, RequestInit?]): Promise<Response> => {
+		const input = args[0];
+		const init = args[1];
+		logger.debug({ input, init }, 'upstream request');
+		const before = new Date();
+		try {
+			// make sure we pass all arguments, since the fetch functions from mesh
+			// do use extra ones
+			const resp = await fetchFn(...args);
+			const text = await resp.text();
+			const duration = new Date().getDate() - before.getDate();
+			logger.debug(
+				{ headers: Object.fromEntries(resp.headers.entries()), status: resp.status, text, duration: `${duration}ms` },
+				'upstream response'
+			);
+			return new Response(text, resp);
+		} catch (error: any) {
+			logger.warn({ error }, 'upstream fetch failed');
+			throw error;
+		}
+	};
+	return fn as F;
+};
