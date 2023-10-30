f83a569a46097d47807652d61814d4ccd8158d85
Pagebakers <eelco@appulse.nl>
2023-10-30 15:09:42 +0100

    fix: pass global fetch to operations client

--- packages/sdk/src/server/server.ts
+++ packages/sdk/src/server/server.ts
@@ -1,5 +1,5 @@
 import closeWithGrace from 'close-with-grace';
-import { Headers } from '@whatwg-node/fetch';
+import { Headers, fetch } from '@whatwg-node/fetch';
 import process from 'node:process';
 import { AsyncLocalStorage } from 'node:async_hooks';
 import Fastify, { FastifyInstance } from 'fastify';
@@ -422,6 +422,7 @@ export const createServer = async ({
 					extraHeaders: headers,
 					tracer: fastify.tracer,
 					traceContext: req.telemetry?.context,
+					customFetch: globalFetch,
 				}),
 				context: await createContext(globalContext),
 			};
