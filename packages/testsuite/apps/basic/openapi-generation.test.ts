56ca9071406fd0be2ede61db192b6b7446c7f5f7
Pagebakers <eelco@appulse.nl>
2023-11-06 16:23:30 +0100

    chore: add test

--- packages/testsuite/apps/basic/openapi-generation.test.ts
+++ packages/testsuite/apps/basic/openapi-generation.test.ts
@@ -3,7 +3,7 @@ import { readFile } from 'node:fs/promises';
 import { join } from 'node:path';
 
 describe('OpenAPI spec generation', () => {
-	it('OpenAPI includes TypeScript operation response schema', async () => {
+	it('OpenAPI includes GraphQL operation response schema', async () => {
 		const filePath = join(__dirname, '.wundergraph', 'generated', 'wundergraph.openapi.json');
 		const data = await readFile(filePath, { encoding: 'utf-8' });
 		const spec = JSON.parse(data);
@@ -17,5 +17,21 @@ describe('OpenAPI spec generation', () => {
 		const responseSchema = response['content']?.['application/json']?.['schema'];
 		expect(responseSchema).toBeDefined();
 		expect(responseSchema['properties']?.['data']?.['type']).toBe('object');
+	});
+
+	it('OpenAPI includes TypeScript operation response schema', async () => {
+		const filePath = join(__dirname, '.wundergraph', 'generated', 'wundergraph.openapi.json');
+		const data = await readFile(filePath, { encoding: 'utf-8' });
+		const spec = JSON.parse(data);
+
+		const countryByFilter = spec.paths?.['/functions/simple'];
+		expect(countryByFilter).toBeDefined();
+
+		const response = countryByFilter?.['get']?.['responses']?.['200'];
+		expect(response).toBeDefined();
+
+		const responseSchema = response['content']?.['application/json']?.['schema'];
+		expect(responseSchema).toBeDefined();
+		expect(responseSchema['properties']?.['data']?.['type']).toBe('string');
 	});
 });
